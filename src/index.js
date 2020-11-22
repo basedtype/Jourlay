605095c687cbe485364d5c37977f9f4c222a9444
Igor Shaposhnikov <jourloy@yandex.ru>
2020-11-23 01:07:10 +0300

    v0.5.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 const { JsonDB } = require('node-json-db');
-const {_, _twitch} = require('./tools');
-const {userClass} = require('./classes');
-const {client, twitch} = require('./twitch');
+const { _, _twitch } = require('./tools');
+const { userClass } = require('./classes');
+const { client, twitch } = require('./twitch');
 
-const nodeDB = new JsonDB(`Data/Channels/setting`, true, true, '/');ƒ
+const nodeDB = new JsonDB(`Data/Channels/setting`, true, true, '/');
+const timers = {
+    ask: 0,
+    hi: 0,
+}
+const twitchInfo = {
+    viewers: 0,
+    maxViewers: 0,
+}
 const arrays = {}
 
 function hiMessage(channel, message, username) {
-    const hi = ['привет', 'хелоу', 'хай', 'куку', 'ку-ку', 'здрасте', 'здрасти', 'здравствуйте', 'здравствуй', 'приветули', 'bonjour', 'бонжур'];
-    const hello = ['привет!', 'приветули!', 'добро пожаловать!', 'вы посмотрите кто пришел!', 'хеллоу!', 'хай!'];
+    if (timers.hi === 0) {
+        const hi = ['привет', 'хелоу', 'хай', 'куку', 'ку-ку', 'здрасте', 'здрасти', 'здравствуйте', 'здравствуй', 'приветули', 'bonjour', 'бонжур'];
+        const hello = ['привет!', 'приветули!', 'добро пожаловать!', 'вы посмотрите кто пришел!', 'хеллоу!', 'хай!'];
 
-    if (_.checkString(message, '@')) return false;
-    if (arrays[channel].hi.includes(username)) return false;
+        if (_.checkString(message, '@')) return false;
+        if (arrays[channel].hi.includes(username)) return false;
 
-    for (let i in hi) {
-        if (_.checkString(message.toLowerCase(), hi[i]) === true) {
-            client.say(channel, `@${username}, ${_.ramdom.elementFromArray(hello)} ShowOfHands ShowOfHands`);
-            arrays[channel].hi.push(username);
-            return true;
+        for (let i in hi) {
+            if (_.checkString(message.toLowerCase(), hi[i]) === true) {
+                client.say(channel, `@${username}, ${_.ramdom.elementFromArray(hello)} ShowOfHands ShowOfHands`);
+                arrays[channel].hi.push(username);
+                timers.hi = 1;
+                const func = () => timers.hi = 0;
+                setTimeout(func, _.convertTime(seconds = 3));
+                return true;
+            }
         }
     }
 
@@ -39,20 +52,25 @@ client.on('action', (channel, userstate, message, self) => {
             return;
         } catch {
             const mod = twitch.isMod(channel, userstate);
-            nodeDB.push(`/${channel}`, {mod: mod});
+            nodeDB.push(`/${channel}`, {
+                mod: mod
+            });
             return;
-        }   
+        }
     }
 
     const username = userstate['display-name'].toLowerCase();
     let user = twitch.db.get(channel, username);
-    if (user === false) user = new userClass({username: username, channel: channel});
+    if (user === false) user = new userClass({
+        username: username,
+        channel: channel
+    });
 
     if (channel === '#jourloy') {
         if (twitch.isMod(channel, userstate)) return;
         twitch.timeout(user, _.convertTime(seconds = 20));
     }
-    
+
     user.message++;
     twitch.db.push(channel, user);
 })
@@ -73,12 +91,59 @@ client.on('message', (channel, userstate, message, self) => {
         }
     }
 
+    if (channel == `#${client.botName}` && messageSplit[0] === '!addCommand') {
+        const command = `!${messageSplit[1]}`;
+        let answer = [];
+        for (let i in messageSplit) if (i > 1) answer.push(messageSplit[i]);
+        answer = answer.join(' ');
+
+        try {
+            const data = nodeDB.getData(`/#${username}_commands`);
+            data.command.push(command);
+            data.answer.push(answer);
+            nodeDB.push(`/#${username}_commands`, {command:  data.command, answer: data.answer});
+        } catch {
+            const comArray = [];
+            comArray.push(command);
+            const answArray = [];
+            answArray.push(answer);
+
+            nodeDB.push(`/#${username}_commands`, {command: comArray, answer: answArray});
+        }
+
+        client.action(channel, `=> команда добавлена. ${command} в своем чате`);
+    }
+
     if (channel == `#${client.botName}` && messageSplit[0] === '!addChannel' && username === 'jourloy') {
         client.addChannel(messageSplit[1]);
         client.action(channel, 'канал добавлен')
     }
 
     if (hiMessage(channel, message, username) === true) return;
+    if (messageSplit[0] === '!q') {
+        const array = ['да!','нет!','возможно','определенно нет','определенно да','50 на 50','шансы есть','без шансов','странный вопрос','я не хочу отвечать','может сменим тему?','не знаю'];
+        if (username === channelName) twitch.say(`@${username}, ${_.ramdom.elementFromArray(array)}`);
+        else if (twitchInfo != null && twitchInfo.viewers < 100) {
+            if (timers.ask === 0 && message.includes('?') && message.length > 6) {
+                client.say(channel, `@${username}, ${_.ramdom.elementFromArray(array)}`);
+                timers.ask = 1;
+                const func = () => timers.ask = 0;
+                setTimeout(func, );
+                twitchInfo.commands++;
+            }
+        }
+        return;
+    }
+
+    try {
+        const data = nodeDB.getData(`/${channel}_commands`);
+        const commands = data.command;
+        const answers = data.answer;
+
+        for (let i in commands) { 
+            if (commands[i] === message) client.say(channel, answers[i]);
+        }
+    } catch {}
 
     if (twitch.mod(channel) === true) modChannel(channel, userstate, message);
     else unmodChannel(channel, userstate, message);
@@ -90,7 +155,10 @@ function modChannel(channel, userstate, message) {
     const username = userstate['display-name'].toLowerCase();
 
     let user = twitch.db.get(channel, username);
-    if (user === false) user = new userClass({username: username, channel: channel});
+    if (user === false) user = new userClass({
+        username: username,
+        channel: channel
+    });
 
     if (_twitch.checkMessage(user, message) === true) return;
 
@@ -103,7 +171,10 @@ function unmodChannel(channel, userstate, message) {
     const username = userstate['display-name'].toLowerCase();
 
     let user = twitch.db.get(channel, username);
-    if (user === false) user = new userClass({username: username, channel: channel});
+    if (user === false) user = new userClass({
+        username: username,
+        channel: channel
+    });
 
     user.message++;
     twitch.db.push(channel, user);
