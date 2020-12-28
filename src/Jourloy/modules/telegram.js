07c887f401a104ab8e94d622e0e7b68182c4d87f
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-28 23:57:08 +0300

    Update telegram.js

--- src/Jourloy/modules/telegram.js
+++ src/Jourloy/modules/telegram.js
@@ -1,10 +1,12 @@
-
 const TelegramBot = require('node-telegram-bot-api');
 const { JsonDB } = require('node-json-db');
+const { _, _twitch } = require('../tools');
 const conf = require('./conf');
 
 const nodeDB = new JsonDB('Data/Users', true, true, '/');
 const bot = new TelegramBot(conf.token, {polling: true});
+_.clearCli()
+console.log('Bot => Telegram => Ready');
 
 class database {
     static create() {
@@ -32,27 +34,31 @@ class telegram {
 
     static notification() {
         const chatID = nodeDB.getData('/chatID').ID;
+        console.log('Bot => Telegram => Noftification => Start');
         for (let i in chatID) {
-            this.send(chatID[i], 'Привет! А тут стрим идет. Залетай давай, все тебя ждут => twitch.com/jourloy')
+            if (chatID !== 466761645) {
+                this.send(chatID[i], 'Ку-ку! Тут стрим запустился, а тебя нет среди зрителей. Залетай => twitch.com/jourloy');
+                console.log(`Bot => Telegram => Noftification => Send (${chatID[i]})`);
+            } else console.log(`Bot => Telegram => Noftification => *Send (466761645)*`);
         }
+        console.log('Bot => Telegram => Noftification => End');
     }
 }
 
-try {
-    const a = nodeDB.getData('/chatID');
-} catch {
-    database.create();
-}
+try { nodeDB.getData('/chatID') } catch { database.create() }
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const message = msg.text.toLowerCase();
-    if (message === '/start') {
-        database.push(chatId);
-        telegram.send(chatId, 'Уведомления подключены')
-    }
-    if (message === '/test') {
-        telegram.notification();
+
+    if (chatId == 466761645) {
+        if (message === '/nf') telegram.notification();
+    } else {
+        if (message === '/start') {
+            database.push(chatId);
+            telegram.send(chatId, 'Уведомления подключены')
+            console.log('Bot => Telegram => Noftification => Add new user');
+        }
     }
 });
 
