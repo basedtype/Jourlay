018c2bf924e51976f52f20a358b70c969228defd
Igor Shaposhnikov <jourloy@yandex.ru>
2021-01-13 15:43:00 +0300

    Update discord.js

--- src/Jourloy/modules/discord.js
+++ src/Jourloy/modules/discord.js
@@ -6,16 +6,82 @@ client.login(config.ds_token);
 
 /* PARAMS */
 
-const announcement = '793404252809986068';
+const channelsID = {
+    noftification: '793404252809986068',
+    moderatorOnly: '748407718414385183',
+    create: {
+        duo: '798853439610159146',
+    }
+}
+const arrays = {
+    channels: [],
+}
 let noftification = null;
 
 /* FUNCTIONS */
 
+function generateString(length) {
+    const chars = '0123456789abcdefghij';
+    let result = '';
+    for (let i = 0; i < length; i++) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
 /* INTERVALS */
 
 setInterval(function () {
+    client.channels.fetch(channelsID.create.duo).then(channel => {
+        if (channel.full == null || channel.full === false) return;
+
+        const parent = channel.parent;
+        const guild = channel.guild;
+        const name = `DUO`;
+        const options = {
+            type: 'voice',
+            userLimit: 2,
+            position: channel.position+1,
+            parent: parent,
+        }
+        
+        let user = channel.members.array()[0].user;
+
+        guild.members.fetch(user.id).then(data => {
+            user = data.guild.voiceStates.cache.array()[0];
+            let idNew = null;
+
+            guild.channels.create(name, options).then(data => {
+                idNew = data.id;
+                let channelNew = undefined;
+                client.channels.fetch(idNew).then(channel => {
+                    channelNew = channel;
+                    user.setChannel(channelNew);
+
+                    const deleteFunc = (channelNew) => {
+                        if (channelNew.members.array().length === 0) {
+                            channelNew.delete();
+                            return true;
+                        }
+                        return false;
+                    }
+
+                    const repeatCheck = function(channelNew) {
+                        setTimeout(function() {deleteChannel(channelNew)}, 1000*1);
+                    }
+
+                    const deleteChannel = function(channelNew) {
+                        if (deleteFunc(channelNew) === false) repeatCheck(channelNew);
+                    };
+
+                    repeatCheck(channelNew);
+                });
+            });
+        })
+    });
+}, 1000);
+
+setInterval(function () {
     if (noftification == null) {
-        client.channels.fetch('748407718414385183').then(channel => {
+        client.channels.fetch(channelsID.noftification).then(channel => {
             if (channel == null) return;
             noftification = channel;
             console.log('Bot => Discord => Ready');
