bc75ec8648ae262695192440d2a5a5fb3cef8511
Игорь Шапошников <jourloy@Igors-iMac.local>
2021-02-07 21:20:59 +0300

    Update Game.js

--- src/Jourloy/Game/Game.js
+++ src/Jourloy/Game/Game.js
@@ -14,11 +14,64 @@ const raid_information = {
 
 /* INTERVALS */
 
+/* CODE */
+
 /* CLASSES */
 
 class Game {
     static help() {
         return '';
+    }
+
+    static repair(client) {
+        const data = Database.get.db();
+        for (let i in data) {
+            if (data[i].game.information.inRaid === true) {
+                const reward = data[i].game.information.raid.reward;
+                const now = Math.floor(moment.now() / 1000);
+                const created_at = data[i].game.information.raid.created;
+                const time = data[i].game.information.raid.time;
+
+                let about = Math.floor((created_at + time) - now);
+                let hours = Math.floor(about/60/60);
+                let minutes = Math.floor(about/60)-(hours*60);
+                let seconds = about%60
+
+                const formatted = [
+                    hours.toString().padStart(2, '0'),
+                    minutes.toString().padStart(2, '0'),
+                    seconds.toString().padStart(2, '0')
+                ].join(':');
+                
+                const startRaid = Database.get.game(i);
+                const raid_start = setTimeout(function() {
+                    if (startRaid.fraction === 'C') client.say(client.channel, `@${i}, боевая задача успешно выполнена, боец группы "Цезарь"! В награду выдаем тебе ${reward.shards} купюр и ${reward.xp} очков опыта`);
+                    if (startRaid.fraction === 'V') client.say(client.channel, `@${i}, посмотрите кто пришел, хахаха. Присаживайся и выпей с нами, викинг! Держи ${reward.shards} золотых монет и ${reward.xp} очков опыта`);
+                    if (startRaid.fraction === 'J') client.say(client.channel, `@${i}, рады видеть тебя, самурай. Пройдем выпьем чаю, а пока в награду мы выдаем тебе ${reward.shards} слитков Великой стали и ${reward.xp} очков опыта`);
+                    if (startRaid.fraction === 'K') client.say(client.channel, `@${i}, спасибо большое, мастер душ, вот тебе ${reward.shards} осколков душ и ${reward.xp} очков опыта`);
+
+                    const endRaid = Database.get.game(i);
+                    endRaid.information.inRaid = false;
+                    endRaid.information.raid.created = 0;
+                    endRaid.information.raid.time = 0;
+                    endRaid.information.raid.reward.shards = 0;
+                    endRaid.information.raid.reward.xp = 0;
+                    endRaid.information.timerID = null;
+                    Database.update.game(i, endRaid);
+
+                    Database.add.shards(i, reward.shards);
+                    Database.add.xp(i, reward.xp);
+
+                }, tools.convertTime({seconds: about}));
+
+                startRaid.information.inRaid = true;
+                startRaid.information.raid.reward.shards = reward.shards;
+                startRaid.information.raid.reward.xp = reward.xp;
+                startRaid.information.timerID = raid_start + " !";
+                Database.update.game(i, startRaid);
+                console.log(`Game => ${i} => Retunr to raid`)
+            }
+        }
     }
  
     static toRaid(username, client) {
@@ -29,17 +82,17 @@ class Game {
         if (user.information.inRaid === true) return errors.ERR_ALREADY_IN_RAID;
         if (user.information.inRest === true) return errors.ERR_USER_IN_REST;
         if (user.hero.wallet < raid_information.price) return errors.ERR_NOT_ENOUGH_SHARDS;
-        Database.remove.shards(username, raid_information.price);
+        Database.remove.wallet(username, raid_information.price);
 
         // TODO LUCKY
         let lucky = 100;
 
         let time = null;
-        if (hero.level <= 1) time = tools.randomInt(1, 2) * 3600;
-        else if (hero.level === 2) time = tools.randomInt(2, 3) * 3600;
-        else if (hero.level === 3) time = tools.randomInt(3, 4) * 3600;
-        else if (hero.level === 4) time = tools.randomInt(4, 5) * 3600;
-        else if (hero.level >= 5) time = tools.randomInt(5, 6) * 3600;
+        if (hero.level <= 1) time = tools.randomInt(1, 2) * tools.randomInt(3600, 5000);
+        else if (hero.level === 2) time = tools.randomInt(2, 3) * tools.randomInt(3600, 5000);
+        else if (hero.level === 3) time = tools.randomInt(3, 4) * tools.randomInt(3600, 5000);
+        else if (hero.level === 4) time = tools.randomInt(4, 5) * tools.randomInt(3600, 5000);
+        else if (hero.level >= 5) time = tools.randomInt(5, 6) * tools.randomInt(3600, 5000);
         if (username === 'jourloy') time = 30;
 
         let hours = Math.floor(time/60/60);
@@ -75,7 +128,7 @@ class Game {
             shards = tools.randomInt(10, 15);
             xp = tools.randomInt(18, 25);
             hp = 0;
-        } else if (hours === 6) {
+        } else if (hours >= 6) {
             shards = tools.randomInt(13, 19);
             xp = tools.randomInt(20, 28);
             hp = 0;
