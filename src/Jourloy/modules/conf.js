373b0d8a9a1d882a2933913abac0aee940fc999a
Igor Shaposhnikov <jourloy@yandex.ru>
2021-02-17 19:07:46 +0300

    Create conf.js

---
+++ src/Jourloy/modules/conf.js
@@ -0,0 +1,3 @@
+exports.tg_token = '1270512300:AAHwV9je0DKqmEmWqV7vlJIZAtjMgPbmOwU';
+exports.ds_token = 'NzM0NjE1ODY5MDExMDAxNDI1.XxUSVw.hiHvaf3FmzjDp9CHQSFU6IGw24Q';
+exports.japanBank_token = 'oauth:byyl8cmaubs3ucz72zkmcm69yq2bhk'
