396177a81135ef812d9468683582c88ce7f860e6
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-02 06:57:36 +0300

    Add repair raids

--- src/modules/game.js
+++ src/modules/game.js
@@ -3,6 +3,217 @@ const moment = require('moment');
 const Discord = require("discord.js");
 
 class Game {
+    static repair(channel, userCollection) {
+        userCollection.find({}).toArray((err, users) => {
+            for (let i in users) {
+                const user = users[i];
+                if (user == null || user.game == null || user.game.inRaid == null || user.game.inRaid === false) return;
+                const username = user.username;
+
+                const created = user.game.raid.created;
+                const timeRaid = user.game.raid.time;
+                const now = Math.floor(moment.now() / 1000);
+                const russian = false;
+                let upd = {
+                    game: user.game
+                }
+
+                if (created+timeRaid < now) {
+                    const xp = user.game.raid.xp;
+                    const wallet = user.game.raid.wallet;
+                    if (russian) {
+                        if (user.game.fraction === 'C') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Возвращение`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Смирно, боец цезарь! Задача выполнена`)
+                            .addFields(
+                                { name: 'Опыт', value: `${xp}`, inline: true},
+                                { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        } else if (user.game.fraction === 'V') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Возвращение`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Посмотрите кто пришел, хахаха. Садись и выпей с нами, викинг!`)
+                            .addFields(
+                                { name: 'Опыт', value: `${xp}`, inline: true},
+                                { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        } else if (user.game.fraction === 'J') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Возвращение`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Я рад видеть тебя, самурай. Пошли выпьем чаю`)
+                            .addFields(
+                                { name: 'Опыт', value: `${xp}`, inline: true},
+                                { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        }
+                    } else {
+                        if (user.game.fraction === 'C') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Come back`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Attention, "Caesar" fighter! Task is done`)
+                            .addFields(
+                                { name: 'XP', value: `${xp}`, inline: true},
+                                { name: 'WM', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        } else if (user.game.fraction === 'V') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Come back`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Look who came, hahaha. Sit down and drink with us, viking!`)
+                            .addFields(
+                                { name: 'XP', value: `${xp}`, inline: true},
+                                { name: 'WM', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        } else if (user.game.fraction === 'J') {
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Come back`)
+                            .setColor(0x00ff00)
+                            .setDescription(`Glad to see you, samurai. Go drink a tea`)
+                            .addFields(
+                                { name: 'XP', value: `${xp}`, inline: true},
+                                { name: 'WM', value: `${wallet}`, inline: true},
+                            )
+                            channel.send(`<@${user.id}>`, {embed: embed});
+                        }
+                    }
+                    console.log(`Twitch => Jourloy_bot => Game => End raid => ${username} => wallet: +${wallet} | xp: ${xp}`);
+                    userCollection.findOne({username: username}).then((user) => {
+                        upd = {
+                            game: user.game
+                        }
+                        upd.game.inRaid = false;
+                        upd.game.wallet = user.game.wallet+wallet;
+                        upd.game.raid = {
+                            created: null,
+                            time: null,
+                            wallet: 0,
+                            xp: 0,
+                        }
+                        upd.game.hero.xp = user.game.hero.xp + xp;
+                        if (upd.game.hero.xp > user.game.hero.level * 100 + (user.game.hero.level * 15)) {
+                            upd.game.hero.level++;
+                            upd.game.hero.xp -= user.game.hero.level * 100 + (user.game.hero.level * 15);
+                        }
+                        userCollection.updateOne({username: username}, {$set: upd});
+                    })
+                } else {
+                    const xp = user.game.raid.xp;
+                    const wallet = user.game.raid.wallet;
+                    const time = created+timeRaid-now;
+                    let hours = Math.floor(time/60/60);
+                    let minutes = Math.floor(time/60)-(hours*60);
+                    let seconds = time%60;
+
+                    const formatted = [
+                        hours.toString().padStart(2, '0'),
+                        minutes.toString().padStart(2, '0'),
+                        seconds.toString().padStart(2, '0')
+                    ].join(':');
+
+                    console.log(`Twitch => Jourloy_bot => Game => Continue raid => ${username} => ${formatted}`);
+
+                    setTimeout(function() {
+                        if (russian) {
+                            if (user.game.fraction === 'C') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Возвращение`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Смирно, боец цезарь! Задача выполнена`)
+                                .addFields(
+                                    { name: 'Опыт', value: `${xp}`, inline: true},
+                                    { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            } else if (user.game.fraction === 'V') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Возвращение`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Посмотрите кто пришел, хахаха. Садись и выпей с нами, викинг!`)
+                                .addFields(
+                                    { name: 'Опыт', value: `${xp}`, inline: true},
+                                    { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            } else if (user.game.fraction === 'J') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Возвращение`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Я рад видеть тебя, самурай. Пошли выпьем чаю`)
+                                .addFields(
+                                    { name: 'Опыт', value: `${xp}`, inline: true},
+                                    { name: 'ЕДМ', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            }
+                        } else {
+                            if (user.game.fraction === 'C') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Come back`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Attention, "Caesar" fighter! Task is done`)
+                                .addFields(
+                                    { name: 'XP', value: `${xp}`, inline: true},
+                                    { name: 'WM', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            } else if (user.game.fraction === 'V') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Come back`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Look who came, hahaha. Sit down and drink with us, viking!`)
+                                .addFields(
+                                    { name: 'XP', value: `${xp}`, inline: true},
+                                    { name: 'WM', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            } else if (user.game.fraction === 'J') {
+                                const embed = new Discord.MessageEmbed()
+                                .setTitle(`Come back`)
+                                .setColor(0x00ff00)
+                                .setDescription(`Glad to see you, samurai. Go drink a tea`)
+                                .addFields(
+                                    { name: 'XP', value: `${xp}`, inline: true},
+                                    { name: 'WM', value: `${wallet}`, inline: true},
+                                )
+                                channel.send(`<@${user.id}>`, {embed: embed});
+                            }
+                        }
+                        console.log(`Twitch => Jourloy_bot => Game => End raid => ${username} => wallet: +${wallet} | xp: ${xp}`);
+                        userCollection.findOne({username: username}).then((user) => {
+                            upd = {
+                                game: user.game
+                            }
+                            upd.game.inRaid = false;
+                            upd.game.wallet = user.game.wallet+wallet;
+                            upd.game.raid = {
+                                created: null,
+                                time: null,
+                                wallet: 0,
+                                xp: 0,
+                            }
+                            upd.game.hero.xp = user.game.hero.xp + xp;
+                            if (upd.game.hero.xp > user.game.hero.level * 100 + (user.game.hero.level * 15)) {
+                                upd.game.hero.level++;
+                                upd.game.hero.xp -= user.game.hero.level * 100 + (user.game.hero.level * 15);
+                            }
+                            userCollection.updateOne({username: username}, {$set: upd});
+                        })
+                    }, Tools.convertTime({seconds: time}));
+                }
+            }
+        });
+    }
+
     static toRaid(username, msg, channel, userCollection, russian) {
         userCollection.findOne({username: username}).then((user) => {
             let hero = user.hero;
@@ -43,7 +254,7 @@ class Game {
             let xp = null;
             let hp = null;
             if (hours <= 1) {
-                wallet = Tools.randomInt(0, 3);
+                wallet = Tools.randomInt(1, 3);
                 xp = Tools.randomInt(1, 10);
                 hp = 0;
             } else if (hours === 2) {
