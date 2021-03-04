49d1ad3189aeea070e0bcae15ea3900f678fc045
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-04 03:06:56 +0300

    Add new function and deprecated class

--- src/Utils/tools.js
+++ src/Utils/tools.js
@@ -19,11 +19,17 @@ const Errors = {
 /* INTERVALS */
 
 /* CLASSES */
-
+/**
+ * @deprecated
+ */
 class Tools {
     static errorHandler(func, options) {
         try { return func(options) }
         catch (error) { return error }
+    }
+
+    static clear() {
+        console.log('\u001B[2J');
     }
 
     static randomInt(min, max) {
@@ -56,7 +62,51 @@ class Tools {
     }
 }
 
-/* EXPORTS */
+class tools {
+    static clear() {
+        console.log('\u001B[2J');
+    }
 
+    /**
+     * 
+     * @param {number} min 
+     * @param {number} max 
+     */
+    static randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    /**
+     * Convert time to ms
+     * @param {{seconds?: number, minutes?:number, hours?:number, days?:number}} time
+     */
+    static convertTime(time) {
+        const seconds = time.seconds || 0;
+        const minutes = time.minutes || 0;
+        const hours = time.hours || 0;
+        const days = time.days || 0;
+        let total = 0;
+
+        total += days*24*60*60;
+        total += hours*60*60;
+        total += minutes*60;
+        total += seconds;
+
+        return total*1000;
+    }
+
+    /**
+     * 
+     * @param {[string]} array 
+     */
+    static randomElementFromArray(array) {
+        return array[this.randomInt(0, array.length-1)];
+    }
+}
+
+/* EXPORTS */
 module.exports.Tools = Tools;
+module.exports.tools = tools;
 module.exports.Errors = Errors;
