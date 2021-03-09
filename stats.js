41c5c89e33264ddce0a35d2e72329cdc9de5698f
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-09 18:15:23 +0300

    Add more information

--- stats.js
+++ stats.js
@@ -1,28 +1,55 @@
 const fs = require('fs');
 const folder = './src/';
 
+const count = {
+    files: 0,
+    lines: 0,
+    comments: 0,
+    space: 0,
+}
+
 class stats {
+    static getLines(file) {
+        const lines = fs.readFileSync(file).toString().split('\n');
+        count.lines += lines.length;
+        this.getComments(lines)
+    }
+
+    static getComments(lines) {
+        let multiComment = false
+        for (let i in lines) {
+            if (lines[i].includes('//')) count.comments++;
+            if (lines[i].includes('/*')) multiComment = true;
+            if (lines[i].includes('*/')) multiComment = false;
+            if (multiComment === true) count.comments++;
+            if (lines[i].length === 0) count.space++;
+        }
+    }
     /**
      * Get lines of project
      */
-    static getLines() {
-        let lines = 0;
+    static getStats() {
         fs.readdirSync(folder).forEach(dirs => {
             if (dirs !== '.DS_Store') {
                 if (dirs.includes('.js') === false) {
                     fs.readdirSync(folder+dirs).forEach(file => {
                         if (file.includes('.js') === false) {
                             fs.readdirSync(folder+dirs+'/'+file).forEach(fileNew => {
-                                lines += fs.readFileSync(folder+dirs+'/'+file+'/'+fileNew).toString().split('\n').length;
+                                count.files++;
+                                this.getLines(folder+dirs+'/'+file+'/'+fileNew);
                             });
                         } else {
-                            lines += fs.readFileSync(folder+dirs+'/'+file).toString().split('\n').length;
+                            count.files++;
+                            this.getLines(folder+dirs+'/'+file);
                         }
-                    });
+                    })
+                } else {
+                    count.files++;
+                    this.getLines(folder+dirs);
                 }
             }
-        });
-        return lines;
+        })
+        return count;
     }
 }
 
