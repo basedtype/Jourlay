1d7d53c7f1a6f4dbd9b0b7e1b23e67c2d1c071f4
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-09 17:49:02 +0300

    Add new defence functions

--- src/modules/defence.js
+++ src/modules/defence.js
@@ -2,7 +2,7 @@
 const { tools } = require('../Utils/tools');
 
 /* PARAMS */
-const version = 'v1.1';
+const version = 'v1.3';
 const ru_alphabet = 'йцукенгшщзхъфывапролджэёячсмитьбюё';
 const defenceLogo = `╔════════════════════════════════════════════════════════════════════╗
 ║      ██████╗░███████╗███████╗███████╗███╗░░██╗░█████╗░███████╗     ║
@@ -18,6 +18,60 @@ console.log(defenceLogo)
 
 /* CLASSES */
 class twitch {
+    static checkBanWords(message, username, client) {
+        let check = false;
+        const bannedWords = ['ниггер', 'нигга', 'пидор', 'черножопый', 'нигретос', 'глиномес', 'пидрила', 'пидорас', 'хиджаб', 'нига', 'хохлы', 'хохол', 'гетвиверс', 'Stream Details',
+            'я бы всех Ни гресов в сарай загнал и сжег нахуй', 'Ez Jebaited followers ', 'хач', 'тестJRJR', '░', '▄', '▀', '▐', '◐', '▇', '⣿', '⢡', '⡤', '⣫', 'bigfollows', '.com', 'W̪', 'n͓', 'путин',
+            'навальный', 'навэльный', 'нэвэльный', 'митинг', 'путинлох', 'свободнаяроссия', 'навальному', 'зиповская', 'карякина', 'байбакова',
+            'niggers', 'nigga', 'niga', 'niger', 'gay', 'putin', 'navalny', 'freerussia', 'zipovskaya', 'baibakova', 'karykina'];
+        const split = message.split(' ');
+
+        for (let i in split) {
+            if (bannedWords.includes(split[i]) === true) check = true;
+        }
+
+        if (check === true) {
+            if (username !== 'jourloy') client.timeout(client.channel, username, 100, 'Banned word');
+            return false;
+        }
+        return true;
+    }
+
+    static checkDeleteWords(message, username, client) {
+        let check = false;
+        const inList = ['pr_'];
+        const list = ['ava', 'cvmask', 'cvhazmat', 'cunt', 'bugger', 'bloody hell', 'bastard', 'wanker', 'toser', 'bollocks', 'retard', 'asshole', 'bitch', 'choad', 'twat'];
+        const splited = message.split(' ');
+
+        for (let i in splited) if (inList.indexOf(splited[i]) !== -1) check = true;
+        for (let i in splited) if (list.includes(splited[i].toLowerCase()) === true) check = true;
+
+        if (check === true) {
+            const id = userstate['id'];
+            if (username !== 'jourloy') client.deletemessage(client.channel, id);
+            return false;
+        }
+        return true;
+    }
+
+    static checkLength(message, username, client) {
+        let check = false
+        const split = message.split(' ');
+        for (let i in split) {
+            let count = 0;
+            for (let j in split) {
+                if (split[j] === split[i]) count++;
+                if (count > 5) check = true;
+            }
+            if (split[i].length > 20) check = true;
+        }
+        if (check === true) {
+            if (username !== 'jourloy') client.timeout(client.channel, username, message.length);
+            return false;
+        }
+        return true;
+    }
+
     static run() {
         const { client } = require('./Bots/Jourloy');
         client.on('message', (channel, userstate, message, self) => {
@@ -30,6 +84,9 @@ class twitch {
                     return;
                 }
             }
+            if (this.checkBanWords(message, username, client) === false) return;
+            if (this.checkDeleteWords(message, username, client) === false) return;
+            if (this.checkLength(message, username, client) === false) return;
         })
     }
 }
