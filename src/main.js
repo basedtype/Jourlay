dbffb896f9e085f9a29537c3d846b4dee4fe5444
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-25 21:48:44 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/**
+ * Main file of Twitch bot
+ */
+
+/* IMPORTS */
+const { colors } = require('./modules/colors');
+
+/* PARAMETERS */
+
+/* CODE */
+
+/* CLASSES */
+class main {
+    static logs = [];
+    static twitch = {
+        commands: false,
+        defence: false,
+    };
+    static discord = {
+        voices: false,
+        defence: false,
+        
+    };
+
+    static run() {
+
+    }
+}
+
