1b4e1c4fc7cd4c81fe78dbaec8fcd7a749210319
Igor Shaposhnikov <jourloy@yandex.ru>
2021-03-26 01:30:20 +0300

    Add conf in database

--- src/modules/Bots/discord.js
+++ src/modules/Bots/discord.js
@@ -1,10 +1,18 @@
 /* IMPORTS */
 const Discord = require("discord.js");
-const config = require('./config')
+const { MongoClient } = require("mongodb");
 
-/* PARAMS */
+/* PARAMETERS */
 const client = new Discord.Client();
-client.login(config.discord);
+const uri = "mongodb://192.168.0.104:12702/";
+const clientDB = new MongoClient(uri);
+
+/* CODE */
+clientDB.connect().then(err => {
+    const database = clientDB.db('config');
+    const config = database.collection('config');
+    config.findOne({name: 'DragonBot', type: 'Discord'}).then(conf => { client.login(conf.oauth) });
+})
 
 /* EXPORTS */
 module.exports.client = client;
