558c69dac6ba8d55309181ea82bad41d8a4c553e
Jourloy <jourloy@yandex.ru>
2021-03-28 17:30:53 +0300

    Use node-fetch module

--- src/modules/uptime.js
+++ src/modules/uptime.js
@@ -1,35 +1,54 @@
+/**
+ * 
+ */
+
 /* IMPORTS */
-const { client } = require('./Bots/twitch');
+const { fetch } = require('../../fetch');
 
 /* PARAMS */
-const uptime = null; 
+const information = {
+    uptime: '',
+    gameHistory: '',
+    viewers: 0,
+    maxViewers: 0,
+    gameWithMaxViewrs: '',
+}
 
 /* INTERVALS */
-setInterval(function() {
-    client.api({
-        url: `https://api.twitch.tv/kraken/streams/158466757`,
-        method: "GET",
+setInterval(() => {
+    const options = {
+        method: 'GET',
         headers: {
             'Accept': 'application/vnd.twitchtv.v5+json',
             "Client-ID": "q9hc1dfrl80y7eydzbehcp7spj6ga1",
             'Authorization': 'OAuth djzzkk9jr9ppnqucmx1ixsce7kl9ly'
         }
-    }, (err, res, body) => {
-        if (body == null || body.stream == null) uptime = null;
-        else if (body != null && body.stream != null) {
-            let now = new Date();
-            let then = body.stream.created_at;
-            let ms = moment(now).diff(moment(then));
-            let d = moment.duration(ms);
-            uptime = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
-        }
+    }
+    fetch.getUserID('jourloy').then(ID => {
+        fetch.get(`https://api.twitch.tv/kraken/streams/${ID}`, options).then(body => {
+            if (body == null || body.stream == null) information.uptime = '';
+            else if (body != null && body.stream != null) {
+                let now = new Date();
+                let then = body.stream.created_at;
+                let ms = moment(now).diff(moment(then));
+                let d = moment.duration(ms);
+                information.uptime = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
+            }
+        })
     })
-}, 1000);
+}, 1000)
 
 /* CLASSES */
-class upt {
-    static get() { return uptime }
+class uptime {
+    static get() {
+        return information;
+    }
+
+    static getFormatted() {
+        if (information.uptime === '') return `Streamer now offline`;
+        else return `Stream uptime is ${information.uptime}`
+    }
 }
 
 /* EXPORTS */
-module.exports.uptime = upt;
+module.exports.uptime = uptime;
