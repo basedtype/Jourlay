19ab04d8b7c36b506057755ba1f04c3813a52da7
Jourloy <jourloy@yandex.ru>
2021-04-09 21:23:23 +0300

    Add new functions and collections

--- src/modules/DBmanager.js
+++ src/modules/DBmanager.js
@@ -1,53 +1,46 @@
 /* IMPORTS */
-const { client } = require("./bots/twitch");
 const DBexamples = require('./DBexamples');
+const { colors } = require('./colors');
 const { MongoClient } = require("mongodb");
 const moment = require('moment');
 
 /* PARAMS */
 const uri = "mongodb://192.168.0.100:12702/";
-const clientDB = new MongoClient(uri);
+const clientDB = new MongoClient(uri, { useUnifiedTopology: true });
 let usersCollection = null;
 let twitchCollection = null;
 
 /* CODE */
 clientDB.connect().then(err => {
-    const database = clientDB.db('DragonBot');
+    database = clientDB.db('Wolfbot');
     usersCollection = database.collection('users');
     twitchCollection = database.collection('twitch');
+    configCollection = database.collection('config');
+    poolCollection = database.collection('pool');
+    namUsersCollection = database.collection('NAMVSEYASNO_users');
+    namGiveCollection = database.collection('NAMVSEYASNO_giveaways');
 })
 
 /* CLASSES */
 class DBmanager {
-    static twitch = {
-        addWatchtime: this._twitchAddWatchtime(username),
-        addWarning: this._twitchAddWarning(username, sender),
-        addDeleteWord: this._twitchAddDeleteWord(word, sender),
-    }
-    static discord = {
-
-    }
-    static global = {
-
-    }
-
     /**
      * Add second in user watchtime
      * @param {string} username 
      */
-    static _twitchAddWatchtime(username) {
-        if (usersCollection == null) return;
-        usersCollection.findOne({ username: username }).then(user => {
+    static _twitchAddWatchtime(username, channel) {
+        if (usersCollection == null) return 'collection is undefined';
+        usersCollection.findOne({ username: username, channel: channel }).then(user => {
             if (user == null) {
-                const docs = DBexamples.twitch.user;
+                const docs = DBexamples.global.user;
                 docs.username = username;
+                docs.channel = channel;
                 usersCollection.insertOne(docs);
-                return;
+                return true;
             }
             user.twitch.watchtime++;
-            usersCollection.findOneAndUpdate({ username: username }, { $set: user });
-            return;
+            usersCollection.findOneAndUpdate({ username: username, channel: channel }, { $set: user });
         })
+        return true;
     }
 
     /**
@@ -56,6 +49,7 @@ class DBmanager {
      * @param {string} sender 
      */
     static _twitchAddWarning(username, sender, reason) {
+        if (usersCollection == null) return 'collection is undefined';
         usersCollection.findOne({ username: username }).then(user => {
             if (user.defence.warnings == null) user.defence.warnings = [];
             const warn = {
@@ -65,16 +59,18 @@ class DBmanager {
             }
             user.defence.warnings.push(warn);
             usersCollection.findOneAndUpdate({ username: username }, { $set: user });
-            return;
         });
+        return true;
     }
 
     /**
      * Add word in delete list
+     * @param {string} username Channel name
      * @param {string} word 
      * @param {string} sender 
      */
-    static _twitchAddDeleteWord(word, sender) {
+    static _twitchAddDeleteWord(username, word, sender) {
+        if (twitchCollection == null) return 'collection is undefined';
         twitchCollection.findOne({ username: username }).then(channel => {
             if (channel.defence.deleteWords == null) channel.defence.deleteWords = [];
             const deleteWord = {
@@ -83,8 +79,45 @@ class DBmanager {
             }
             channel.defence.deleteWords.push(deleteWord);
             twitchCollection.findOneAndUpdate({ username: username }, { $set: channel });
-            return;
         });
+        return true;
+    }
+
+    /**
+     * 
+     * @param {string} username 
+     * @param {string} type 
+     * @param {string} oauth 
+     * @returns 
+     */
+    static _configAddBot(username, type, oauth) {
+        if (configCollection == null) return 'collection is undefined';
+        if (username == null) username = '';
+        if (type == null) return 'Need <type> for bot'
+        if (oauth == null) return 'Need <oauth> for bot'
+        const docs = DBexamples.config.bot;
+        docs.username = username;
+        docs.type = type;
+        docs.oauth = oauth;
+        configCollection.insertOne(docs)
+        return true;
+    }
+
+    /**
+     * Add new block in pool
+     * @param {string} type 
+     * @param {string} destination 
+     * @param {} data 
+     * @param {number} priority 
+     * @returns 
+     */
+    static _poolAddlBlock(type, owner, doing) {
+        const block = DBexamples.pool.block;
+        block.type = type,
+            block.owner = owner;
+        block.do = doing;
+        poolCollection.insertOne(block);
+        return true;
     }
 }
 
