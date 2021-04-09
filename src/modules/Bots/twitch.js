b5973c2e92dfe8a14db3a6a9263ab61945371257
Jourloy <jourloy@yandex.ru>
2021-04-09 21:22:01 +0300

    Add waiting connection and param for MongoClient

--- src/modules/Bots/twitch.js
+++ src/modules/Bots/twitch.js
@@ -2,38 +2,44 @@
 const tmi = require('tmi.js');
 const { MongoClient } = require("mongodb");
 
-/* PARAMETERS */
-const options = {
-    options: { debug: false },
-    connection: {
-        cluster: 'aws',
-        reconnect: true
-    },
-    identity: {
-        username: 'jourlay',
-        password: null,
-    },
-    channels:['#jourloy'],
-};
-const uri = "mongodb://192.168.0.104:12702/";
-const clientDB = new MongoClient(uri);
-const client = new tmi.client(options);
+/* CLASSES */
+class twitch {
+    static client = null;
 
-/* FUNCTIONS */
-function onConnectedHandler() { client.color("OrangeRed") }
+    static create(channel) {
+        const options = {
+            options: { debug: false },
+            connection: {
+                cluster: 'aws',
+                reconnect: true
+            },
+            identity: {
+                username: 'jourlay',
+                password: null,
+            },
+            channels:[`#${channel}`],
+        };
+        const uri = "mongodb://192.168.0.100:12702/";
+        const clientDB = new MongoClient(uri, { useUnifiedTopology: true });
+        return clientDB.connect().then(err => {
+            const database = clientDB.db('Wolfbot');
+            const config = database.collection('config');
+            config.findOne({name: 'Wolfbot', type: 'Twitch'}).then(conf => {
+                options.identity.password = conf.oauth;
+                this.client = new tmi.client(options);
+                this.client.connect();
+            });
+        })
+    }
+    
+    static connect(channel) {
+        this.create(channel);
+    }
 
-/* CODE */
-clientDB.connect().then(err => {
-    const database = clientDB.db('config');
-    const config = database.collection('config');
-    config.findOne({name: 'DragonBot', type: 'Twitch'}).then(conf => {
-        options.identity.password = conf.oauth;
-        client.connect();
-    });
-})
-
-/* REACTIONS */
-client.on('connected', onConnectedHandler);
+    static update() {
+        return this.client;
+    }
+}
 
 /* EXPORTS */
-module.exports.client = client;
+module.exports.twitch = twitch;
