b61b3f3715bca677eff63a5aed2141e3dccd78d0
Jourloy <jourloy@yandex.ru>
2021-04-11 19:52:43 +0300

    Fix css error

--- src/modules/server.js
+++ src/modules/server.js
@@ -7,12 +7,14 @@ const port = 80;
 const server = http.createServer((request, response) => {
     let file = `./src/modules/site${request.url}`;
     try {
+        let filePath = `./src/modules/site${request.url}`;
         const urlSplit = request.url.split('.');
         const file = urlSplit[urlSplit.length - 1];
+        console.log(file)
         const header = `text/${file}`
         response.setHeader('Content-Type', header)
         response.statusCode = 200;
-        response.end(fs.readFileSync(file));
+        response.end(fs.readFileSync(filePath));
     } catch {
         response.setHeader('Content-Type', 'text/plain')
         response.statusCode = 200;
