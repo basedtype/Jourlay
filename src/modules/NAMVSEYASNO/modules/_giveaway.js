91e99e928423785fd372a93c5bfd88eba3873476
Jourloy <jourloy@yandex.ru>
2021-04-11 19:28:03 +0300

    Add config for giveaways

---
+++ src/modules/NAMVSEYASNO/modules/_giveaway.js
@@ -0,0 +1,15 @@
+/* CLASSES */
+class giveaway {
+    static config = {
+        start: 'gstart',
+        end: 'gend',
+        reroll: 'greroll',
+        nameRole: 'giveaway',
+        nameChannel: 'розыгрыш'
+    }
+
+
+}
+
+/* EXPORTS */
+module.exports.giveaway = giveaway;
