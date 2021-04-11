592ac40eaec7b09f9323f8ee657d51223f3efd75
Jourloy <jourloy@yandex.ru>
2021-04-11 19:33:48 +0300

    Fix fs error

--- src/modules/server.js
+++ src/modules/server.js
@@ -8,7 +8,7 @@ const server = http.createServer((request, response) => {
     if (request.url === '/nidhoggbot/ru/giveaway') {
         response.setHeader('Content-Type', 'text/html')
         response.statusCode = 200;
-        response.end('./src/modules/site/nidhoggbot/ru/giveaway.html');
+        response.end(fs.readFileSync('./src/modules/site/nidhoggbot/ru/giveaway.html'));
     } else {
         response.setHeader('Content-Type', 'text/plain')
         response.statusCode = 200;
