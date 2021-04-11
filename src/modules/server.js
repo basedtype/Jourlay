bcaba0e390d89fb5b27bcc502042eb16f8f60d78
Jourloy <jourloy@yandex.ru>
2021-04-11 19:40:51 +0300

    Try to fix css error

--- src/modules/server.js
+++ src/modules/server.js
@@ -5,11 +5,12 @@ const hostname = '192.168.0.106';
 const port = 80;
 
 const server = http.createServer((request, response) => {
-    if (request.url === '/nidhoggbot/ru/giveaway') {
+    let file = `./src/modules/site${request.url}`;
+    try {
         response.setHeader('Content-Type', 'text/html')
         response.statusCode = 200;
-        response.end(fs.readFileSync('./src/modules/site/nidhoggbot/ru/giveaway.html'));
-    } else {
+        response.end(fs.readFileSync(file));
+    } catch {
         response.setHeader('Content-Type', 'text/plain')
         response.statusCode = 200;
         response.end('Lol what?');
