5a1ef0a938d793c7ab8fa1c1550cd4b1efa4e8e3
Jourloy <jourloy@yandex.ru>
2021-04-11 19:27:29 +0300

    Rename bot

--- src/modules/Bots/discord.js
+++ src/modules/Bots/discord.js
@@ -9,9 +9,9 @@ const clientDB = new MongoClient(uri, { useUnifiedTopology: true });
 
 /* CODE */
 clientDB.connect().then(err => {
-    const database = clientDB.db('Wolfbot');
+    const database = clientDB.db('Nidhoggbot');
     const config = database.collection('config');
-    config.findOne({name: 'Wolfbot', type: 'Discord'}).then(conf => { client.login(conf.oauth) });
+    config.findOne({name: 'Nidhoggbot', type: 'Discord'}).then(conf => { client.login(conf.oauth) });
 })
 
 /* EXPORTS */
