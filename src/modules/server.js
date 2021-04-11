1e4c66eaa88d260c7128721fef6ba6b898da07bc
Jourloy <jourloy@yandex.ru>
2021-04-11 19:28:20 +0300

    Add page

--- src/modules/server.js
+++ src/modules/server.js
@@ -1,14 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '192.168.0.106';
 const port = 80;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain')
-  response.end('Hello World');
+    if (request.url === '/nidhoggbot/ru/giveaway') {
+        response.setHeader('Content-Type', 'text/html')
+        response.statusCode = 200;
+        response.end('./src/modules/site/nidhoggbot/ru/giveaway.html');
+    } else {
+        response.setHeader('Content-Type', 'text/plain')
+        response.statusCode = 200;
+        response.end('Lol what?');
+    }
+    
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
