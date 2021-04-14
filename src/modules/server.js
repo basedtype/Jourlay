ab386aa9511fb8605daa565bc9bb3e9a36126f8d
Jourloy <jourloy@yandex.ru>
2021-04-14 03:30:28 +0300

    Moved server on expressJS

--- src/modules/server.js
+++ src/modules/server.js
@@ -1,67 +1,88 @@
 /* IMPORTS */
 const { DBmanager } = require('./DBmanager');
 const { colors } = require('./colors');
-const http = require('http');
+const express = require('express')
 const fs = require('fs');
 
 /* PARAMS */
+const app = express()
 const hostname = '192.168.0.106';
 const port = 80;
 const banCount = 30;
 const allowList = ['127.0.0.1', '192.168.0.106']
 
-/* FUNCTIONS */
-const server = http.createServer((request, response) => {
-    const requestIP = request.socket.server._connectionKey.split(':')[1];
-    DBmanager._serverIPGet(requestIP).then(ipAddress => {
-        if (ipAddress == null) {
-            try {
-                let filePath = `./src/modules/site${request.url}`;
-                const urlSplit = request.url.split('.');
-                const file = urlSplit[urlSplit.length - 1];
-                const header = `text/${file}`
-                response.setHeader('Content-Type', header)
-                response.statusCode = 200;
-                response.end(fs.readFileSync(filePath));
-            } catch {
-                if (request.url !== '/') DBmanager._serverIPAdd(requestIP);
-                response.setHeader('Content-Type', 'text/plain')
-                response.statusCode = 404;
-                response.end('Lol what?');
-            }
-        } else {
-            if (ipAddress.ban === true) {
+/* REACTIONS */
+/**
+ * Stop banned trafic
+ */
+app.use((request, response, next) => {
+    const requestIP = request.ip.split(':').pop();
+    DBmanager._serverIPGetBanned().then(ipAddress => {
+        ipAddress.toArray((err, res) => {
+            for (let i in res) if (res[i].ip === requestIP) {
                 console.log(colors.get(`[WARNING] request from banned IP address was stopped`, 'FgYellow'));
                 return;
             }
-            if (ipAddress.count > banCount) {
-                response.setHeader('Content-Type', 'text/plain')
-                response.statusCode = 403;
-                response.end('You are banned');
-                DBmanager._serverIPban(requestIP);
-                console.log(colors.get(`[WARNING] IP address was banned {${requestIP}}`, 'FgYellow'));
-                return;
-            }
-            try {
-                let filePath = `./src/modules/site${request.url}`;
-                const urlSplit = request.url.split('.');
-                const file = urlSplit[urlSplit.length - 1];
-                const header = `text/${file}`
-                response.setHeader('Content-Type', header)
-                response.statusCode = 200;
-                response.end(fs.readFileSync(filePath));
-            } catch {
-                if (request.url !== '/' && allowList.includes(requestIP) === false) DBmanager._serverIPAdd(requestIP, 3);
-                if (request.url === '/' && allowList.includes(requestIP) === false) DBmanager._serverIPAdd(requestIP, 1);
-                response.setHeader('Content-Type', 'text/plain')
-                response.statusCode = 404;
-                response.end('Lol what?');
-            }
-        }
+            next();
+        })
     })
-});
+})
 
-/* REACTIONS */
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+/**
+ * Add warnings for IP address
+ */
+app.use((request, response, next) => {
+    const requestIP = request.ip.split(':').pop();
+    const requestURL = request.url;
+    let filePath = `./src/modules/site${requestURL}`;
+    try {
+        fs.readFileSync(filePath);
+    } catch {
+        if (request.url !== '/' && allowList.includes(requestIP) === false) DBmanager._serverIPAdd(requestIP, 3);
+        if (request.url === '/' && allowList.includes(requestIP) === false) DBmanager._serverIPAdd(requestIP, 1);
+    }
+    next();
+})
+
+/**
+ * Ban IP address with limit warnings
+ */
+app.use((request, response, next) => {
+    const requestIP = request.ip.split(':').pop();
+    DBmanager._serverIPGet(requestIP).then(ipAddress => {
+        if (ipAddress.count > banCount) {
+            DBmanager._serverIPban(requestIP);
+            console.log(colors.get(`[WARNING] IP address was banned {${requestIP}}`, 'FgYellow'));
+            return;
+        }
+        next();
+    })
+})
+
+/**
+ * Response page
+ */
+app.use((request, response, next) => {
+    try {
+        let filePath = `./src/modules/site${request.url}`;
+        const urlSplit = request.url.split('.');
+        const file = urlSplit[urlSplit.length - 1];
+        const header = `text/${file}`
+        response.setHeader('Content-Type', header)
+        response.statusCode = 200;
+        response.end(fs.readFileSync(filePath));
+    } catch {
+        response.setHeader('Content-Type', 'text/plain')
+        response.statusCode = 404;
+        response.end('');
+    }
+    next();
+})
+
+/**
+ * Start a server
+ */
+app.listen(port, hostname, (err) => {
+    if (err) return console.log('something bad happened', err);
+    console.log(`server is listening on ${port}`)
+})
