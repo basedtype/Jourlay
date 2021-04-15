3dbe3e7f7e546f1a259f4b669c2b8f442d79f5ee
Jourloy <jourloy@yandex.ru>
2021-04-15 03:25:03 +0300

    Add eve support, 404 page, better protect and api support

--- src/modules/server.js
+++ src/modules/server.js
@@ -3,19 +3,21 @@ const { DBmanager } = require('./DBmanager');
 const { colors } = require('./colors');
 const express = require('express')
 const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const fetch = require('node-fetch');
 
 /* PARAMS */
 const app = express()
-const hostname = '192.168.0.106';
+const hostname = '77.66.178.141';
 const port = 80;
 const banCount = 30;
 const allowList = ['127.0.0.1', '192.168.0.106', '77.66.178.141']
 
 /* REACTIONS */
 /**
- * Check IP address in database and and if need
+ * Check IP address in database and add if need
  */
- app.use((request, response, next) => {
+app.use((request, response, next) => {
     const requestIP = request.ip.split(':').pop();
     DBmanager._serverIPGet(requestIP).then(ipAddress => {
         if (ipAddress === null) DBmanager._serverIPAdd(requestIP);
@@ -77,23 +79,132 @@ app.use((request, response, next) => {
 })
 
 /**
- * Response page
+ * Response css and etc files for server
  */
 app.use((request, response, next) => {
-    try {
+    const requestIP = request.ip.split(':').pop();
+    const urlSplit = request.url.split('.');
+    const file = urlSplit[urlSplit.length - 1];
+    if ((file === 'css' || file === 'js') && allowList.includes(requestIP) === true) {
         let filePath = `./src/modules/site${request.url}`;
-        const urlSplit = request.url.split('.');
-        const file = urlSplit[urlSplit.length - 1];
         const header = `text/${file}`
         response.setHeader('Content-Type', header)
         response.statusCode = 200;
         response.end(fs.readFileSync(filePath));
-    } catch {
-        response.setHeader('Content-Type', 'text/plain')
-        response.statusCode = 404;
-        response.end('');
+    } else next();
+})
+
+/**
+ * COOKIE PARSER 
+ */
+app.use(cookieParser('NqiqDq'));
+app.use(express.urlencoded({ extended: false }))
+
+/**
+ * Nidhoggbot discord giveaway documentation
+ */
+app.get('/nidhoggbot/ru/giveaway.html', function (request, response, next) {
+    //response.cookie('remember', 1, { maxAge: 60000 });
+    //console.log(request.cookies)
+    let filePath = `./src/modules/site${request.url}`;
+    const urlSplit = request.url.split('.');
+    const file = urlSplit[urlSplit.length - 1];
+    const header = `text/${file}`
+    response.setHeader('Content-Type', header)
+    response.statusCode = 200;
+    response.end(fs.readFileSync(filePath));
+});
+
+app.get('/eve/authErr.html', function (request, response, next) {
+    //response.cookie('remember', 1, { maxAge: 60000 });
+    //console.log(request.cookies)
+    let filePath = `./src/modules/site${request.url}`;
+    const urlSplit = request.url.split('.');
+    const file = urlSplit[urlSplit.length - 1];
+    const header = `text/${file}`
+    response.setHeader('Content-Type', header)
+    response.statusCode = 200;
+    response.end(fs.readFileSync(filePath));
+});
+
+/**
+ * Nidhoggbot eve auth success
+ */
+ app.get('/eve/auth.html', function (request, response, next) {
+    let filePath = `./src/modules/site${request.url}`;
+    const urlSplit = request.url.split('.');
+    const file = urlSplit[urlSplit.length - 1];
+    const header = `text/${file}`
+    response.setHeader('Content-Type', header)
+    response.statusCode = 200;
+    response.end(fs.readFileSync(filePath));
+});
+
+/**
+ * Nidhoggbot eve auth redirect
+ */
+app.get('/eve/auth', function (request, response, next) {
+    if (request.query.username == null) response.redirect(`http://${hostname}/eve/authErr.html#queryUsernameUndefined`);
+    else {
+        let scopes = [
+            'esi-characters.read_blueprints.v1',
+            'esi-wallet.read_character_wallet.v1',
+            'esi-wallet.read_corporation_wallets.v1',
+            'esi-ui.write_waypoint.v1',
+            'esi-ui.open_window.v1',
+            'esi-mail.read_mail.v1',
+            'esi-mail.organize_mail.v1',
+            'esi-mail.send_mail.v1',
+            'esi-markets.read_character_orders.v1',
+            'esi-markets.read_corporation_orders.v1',
+            'esi-markets.structure_markets.v1',
+        ]
+        scopes = scopes.join('%20')
+        response.cookie('username', request.query.username, { maxAge: 60000 });
+        response.redirect(`https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=http://${hostname}/eve/callback&client_id=67655ff739984b6cbab0782118bcbd2f&scope=${scopes}&state=NqiqDq`);
     }
-    next();
+});
+
+/**
+ * Nidhoggbot eve auth redirect
+ */
+app.get('/eve/callback', function (request, response, next) {
+    if (request.query.code == null) response.redirect(`http://${hostname}/eve/authErr.html#authCodeUndefined`)
+    else {
+        const options = {
+            method: "POST",
+            headers: {
+                "Authorization": "Basic Njc2NTVmZjczOTk4NGI2Y2JhYjA3ODIxMThiY2JkMmY6U3djc2J2SEtlR2lMNXRGUHNqVUo3dDZaYUplaW5DcGJZOEN3SThWYw==",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Host": "login.eveonline.com"
+            },
+            body: `grant_type=authorization_code&code=${request.query.code}`
+        }
+        
+        //test(options)
+        fetch(`https://login.eveonline.com/v2/oauth/token`, options).then(res => {
+            res.json().then(data => {
+                const username = request.cookies.username;
+                const accessToken = data.access_token;
+                const refreshToken = data.refresh_token;
+                DBmanager._eveAdduser(username, accessToken, refreshToken);
+                response.redirect(`http://${hostname}/eve/auth.html`)
+            })
+        });
+    }
+});
+
+app.get('/api', function (request, response, next) {
+    const query = request.query;
+});
+
+/**
+ * Error handler for 404 error
+ */
+app.use((request, response, next) => {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html')
+    response.end(fs.readFileSync('./src/modules/site/404.html'));
 })
 
 /**
