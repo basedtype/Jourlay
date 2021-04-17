70b85b7a1ba5bf586b5829f21af5bb7609387100
Jourloy <jourloy@yandex.ru>
2021-04-17 04:09:14 +0300

    Add toggle form and ajax

--- src/modules/site/js/index.js
+++ src/modules/site/js/index.js
@@ -1,3 +1,47 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+/* function showError() {
+    const incorrect = document.getElementById('incorrect');
+    incorrect.style.display = 'block';
+    incorrect.style.visibility = 'visible';
+} */
+
+jQuery(function ($) {
+    $('.message a').on('click', element => {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+        $('incorrect').animate({display: "inline"})
+    });
+
+    $('#login-form').on('submit', event => {
+        var $data = {};
+        $('#login-form').find ('input, textearea, select').each(function() {
+            $data[this.name] = $(this).val();
+        });
+        $.ajax({
+            url: "/login",
+            data: {
+              type: 'login',
+              username: $data.username,
+              password: $data.password
+            },
+            success: function( result ) {
+                const check = result.split(' ');
+                if (check[0] === 'Success') window.location.href = check[1];
+                else {
+                    $('#err').html(result);
+                    var incorrect = document.getElementById('incorrect');
+                    incorrect.style.display = 'block';
+                    incorrect.style.visibility = 'visible';
+                }
+            }
+        });
+        event.preventDefault();
+    });
+
+    $('#register-form').on('submit', event => {
+        var $data = {};
+        $('#register-form').find ('input, textearea, select').each(function() {
+            $data[this.name] = $(this).val();
+        });
+        console.log($data)
+        event.preventDefault();
+    });
+});
