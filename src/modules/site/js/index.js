5714398102675c9bcf9a328962102da3d07ee450
Jourloy <jourloy@yandex.ru>
2021-04-18 22:39:57 +0300

    Remove login form and add donate page

--- src/modules/site/js/index.js
+++ src/modules/site/js/index.js
@@ -1,47 +1,87 @@
-/* function showError() {
-    const incorrect = document.getElementById('incorrect');
-    incorrect.style.display = 'block';
-    incorrect.style.visibility = 'visible';
-} */
-
 jQuery(function ($) {
-    $('.message a').on('click', element => {
-        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
-        $('incorrect').animate({display: "inline"})
-    });
 
-    $('#login-form').on('submit', event => {
-        var $data = {};
-        $('#login-form').find ('input, textearea, select').each(function() {
-            $data[this.name] = $(this).val();
-        });
-        $.ajax({
-            url: "/login",
-            data: {
-              type: 'login',
-              username: $data.username,
-              password: $data.password
-            },
-            success: function( result ) {
-                const check = result.split(' ');
-                if (check[0] === 'Success') window.location.href = check[1];
-                else {
-                    $('#err').html(result);
-                    var incorrect = document.getElementById('incorrect');
-                    incorrect.style.display = 'block';
-                    incorrect.style.visibility = 'visible';
+    function changeWallet(donationData) {
+        $('#donationData').fadeOut(100);
+        setTimeout(function() {$('#donationData').html(donationData)}, 100);
+        $('#donationData').fadeIn(400);
+    }
+
+    /**
+     * 
+     * @param {ElementCSSInlineStyle} doc 
+     * @returns 
+     */
+    function returnButton(doc) {
+        const style = doc.style;
+        style.backgroundColor = "rgb(87, 20, 20)";
+    }
+
+    /**
+     * 
+     * @param {ElementCSSInlineStyle} doc 
+     * @returns 
+     */
+    function colorButton(doc) {
+        const style = doc.style;
+        style.backgroundColor = "rgb(233, 54, 54)";
+    }
+
+    let $donationData = '0xb2bfd953fe794287702369aaae672c8a7f17ae15';
+    $('#donationData').html($donationData);
+    $('#ETHbutton').css('background-color', 'rgb(233, 54, 54)')
+    let $donationButtonActive = 1;
+
+    $('.donationButton').on('click', element => {
+        const buttonID = element.currentTarget.id;
+        switch (buttonID) {
+            case 'ETHbutton':
+                if ($donationButtonActive !== 1) {
+                    if ($donationButtonActive === 2) returnButton(document.getElementById('BTCbutton'));
+                    if ($donationButtonActive === 3) returnButton(document.getElementById('BANKbutton'));
+                    if ($donationButtonActive === 4) returnButton(document.getElementById('PAYPALbutton'));
+                    colorButton(document.getElementById('ETHbutton'));
+                    $donationData = '0xb2bfd953fe794287702369aaae672c8a7f17ae15';
+                    $donationButtonActive = 1;
+                    changeWallet($donationData);
                 }
-            }
-        });
-        event.preventDefault();
-    });
+                break;
+            case 'BTCbutton':
+                if ($donationButtonActive !== 2) {
+                    if ($donationButtonActive === 1) returnButton(document.getElementById('ETHbutton'));
+                    if ($donationButtonActive === 3) returnButton(document.getElementById('BANKbutton'));
+                    if ($donationButtonActive === 4) returnButton(document.getElementById('PAYPALbutton'));
+                    colorButton(document.getElementById('BTCbutton'));
+                    $donationData = '1DDv93oScG8oSxGsKnQPyj4rw4WWigoGHe';
+                    $donationButtonActive = 2;
+                    changeWallet($donationData);
+                }
+                break;
+            case 'BANKbutton':
+                if ($donationButtonActive !== 3) {
+                    if ($donationButtonActive === 1) returnButton(document.getElementById('ETHbutton'));
+                    if ($donationButtonActive === 2) returnButton(document.getElementById('BTCbutton'));
+                    if ($donationButtonActive === 4) returnButton(document.getElementById('PAYPALbutton'));
+                    colorButton(document.getElementById('BANKbutton'));
+                    $donationData = 'soon';
+                    $donationButtonActive = 3;
+                    changeWallet($donationData);
+                }
+                break;
+            case 'PAYPALbutton':
+                if ($donationButtonActive !== 4) {
+                    if ($donationButtonActive === 1) returnButton(document.getElementById('ETHbutton'));
+                    if ($donationButtonActive === 3) returnButton(document.getElementById('BANKbutton'));
+                    if ($donationButtonActive === 2) returnButton(document.getElementById('BTCbutton'));
+                    colorButton(document.getElementById('PAYPALbutton'));
+                    $donationData = '@jourloy';
+                    $donationButtonActive = 4;
+                    changeWallet($donationData);
+                }
+                break;
+        }
+    })
 
-    $('#register-form').on('submit', event => {
-        var $data = {};
-        $('#register-form').find ('input, textearea, select').each(function() {
-            $data[this.name] = $(this).val();
-        });
-        console.log($data)
-        event.preventDefault();
-    });
+    $('.toHome').on('click', element => {
+        window.location.replace('/')
+    })
 });
