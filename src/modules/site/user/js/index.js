56aefe27237584b5c52e15e8377469edf86eaf68
Jourloy <jourloy@yandex.ru>
2021-04-19 08:32:34 +0300

    Change api url and redirect url

--- src/modules/site/user/js/index.js
+++ src/modules/site/user/js/index.js
@@ -3,14 +3,13 @@ jQuery(function ($) {
         $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
         $('incorrect').animate({display: "inline"})
     });
-
     $('#login-form').on('submit', event => {
         var $data = {};
         $('#login-form').find ('input, textearea, select').each(function() {
             $data[this.name] = $(this).val();
         });
         $.ajax({
-            url: "/login",
+            url: "/api/login",
             data: {
               type: 'login',
               username: $data.username,
@@ -18,7 +17,11 @@ jQuery(function ($) {
             },
             success: function( result ) {
                 const check = result.split(' ');
-                if (check[0] === 'Success') window.location.href = check[1];
+                if (check[0] === 'Success') {
+                    let uri = window.location.hash;
+                    uri = uri.split('#')[1];
+                    window.location.href = `/${uri}/`;
+                }
                 else {
                     $('#err').html(result);
                     var incorrect = document.getElementById('incorrect');
