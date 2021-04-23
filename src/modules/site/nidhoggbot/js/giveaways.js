981256f1363c60f57e435dc37707531709c5e879
Jourloy <jourloy@yandex.ru>
2021-04-23 15:21:43 +0300

    Create giveaways.js

---
+++ src/modules/site/nidhoggbot/js/giveaways.js
@@ -0,0 +1,51 @@
+jQuery(function ($) {
+    $('#create-form').on('submit', event => {
+        var $data = {};
+        $('#create-form').find('input, textearea, select').each(function () {
+            $data[this.name] = $(this).val();
+        });
+        $data['webUsername'] = $.cookie('username');
+        console.log($data.webUsername)
+        $.ajax({
+            url: "/api/config/user",
+            data: {
+                type: `check_access`,
+                username: $data.webUsername,
+                guildID: $data.guildID,
+            },
+            success: function (result) {
+                const check = result.split(' ');
+                if (check[0] === 'Success') {
+                    $.ajax({
+                        url: "/api/giveaway/create",
+                        data: {
+                            guildID: $data.guildID,
+                            title: $data.title,
+                            time: $data.time,
+                            amount: $data.amount,
+                            urlTitle: $data.urlTitle,
+                            urlImage: $data.urlImage,
+                            webUsername: $data.webUsername,
+                        },
+                        success: function (result) {
+                            const check = result.split(' ');
+                            if (check[0] === 'Success') window.location.href = `/nidhoggbot/`;
+                            else {
+                                $('#err').html(result);
+                                var incorrect = document.getElementById('incorrect');
+                                incorrect.style.display = 'block';
+                                incorrect.style.visibility = 'visible';
+                            }
+                        }
+                    });
+                } else {
+                    $('#err').html(result);
+                    var incorrect = document.getElementById('incorrect');
+                    incorrect.style.display = 'block';
+                    incorrect.style.visibility = 'visible';
+                }
+            }
+        });
+        event.preventDefault();
+    });
+});
