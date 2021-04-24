2900e52da745f83238d6ebd071d554cad5618fd8
Jourloy <jourloy@yandex.ru>
2021-04-24 18:20:43 +0300

    Recode

--- src/modules/tools/discord.js
+++ src/modules/tools/discord.js
@@ -6,11 +6,13 @@ const { discordLog } = require('./discordTools');
 const Discord = require("discord.js");
 
 /* PARAMS */
+const giveaways = [];
 
 /* REACTIONS */
 client.on('message', msg => {
     if (msg.content === '!nidhoggbot giveaway') {
         DBmanager._giveawayAddChannelInGuild(msg.guild.id, msg.channel.id, msg.author.id);
         discordLog.success(msg.channel, `Channel added in giveaway database`, msg.guild.owner);
+        msg.delete();
     }
 })
