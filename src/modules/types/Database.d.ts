626f5b1fdf7b23c54eb18a1db7639142b7218d8f
Jourloy <jourloy@yandex.ru>
2021-05-12 11:59:20 +0300

    Add new types

--- src/modules/types/Database.d.ts
+++ src/modules/types/Database.d.ts
@@ -9,6 +9,10 @@ declare namespace config {
         type: string;
         ID: number;
         access: config.access;
+        giveaways?: {
+            channelID: string;
+            userAccess: string[]
+        }
     }
     
     interface access {
@@ -26,8 +30,70 @@ declare namespace server {
     }
 }
 
-interface poolBlock {
-    type: string;
-    owner: string;
-    data: string;
+declare namespace pool {
+    interface logBlock {
+        type: string;
+        owner: string;
+        text: string;
+        logging: boolean;
+        color?: string;
+    }
 }
+
+declare namespace giveaways {
+    interface give {
+        msgID: string;
+        /**
+         * Amount of winners
+         */
+        amount: number;
+        /**
+         * Time length in ms
+         * @deprecated use .time
+         */
+        length: number;
+        time?: string;
+        /**
+         * Time of end in ms
+         */
+        end: number;
+        /**
+         * Time of created in ms
+         */
+        created: number;
+        /**
+         * Who join in giveaway
+         */
+        people: string[];
+        /**
+         * Url in title
+         */
+        urlTitle: string;
+        /**
+         * Url on image in body
+         */
+        urlImage: string;
+        title: string;
+        /**
+         * Who create giveaway
+         * @deprecated
+         */
+        authorUsername?: string;
+        /**
+         * Url on author's avatar
+         * @deprecated
+         */
+        authorURL?: string;
+        owner?: string;
+        /**
+         * TODO: Resolve ^ and v
+         */
+        guildID?: number;
+    }
+}
+
+interface configUser {
+    username: string;
+    giveaways?: string[]
+} 
+
