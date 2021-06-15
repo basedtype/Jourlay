7254a7dd7e90cedb77d8f00be412a6461141f3b8
Jourloy <jourloy@yandex.ru>
2021-06-16 02:25:22 +0300

    Add time format

--- src/modules/tools/main.ts
+++ src/modules/tools/main.ts
@@ -12,4 +12,24 @@ export class tools {
         if (time.mounths != null) totalTime += time.mounths * 31 * 24 * 60 * 60 * 1000;
         return totalTime;
     }
+
+    public static toHHMMSS(second) {
+        const sec_num = parseInt(second, 10);
+        const hours   = Math.floor(sec_num / 3600);
+        const minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        const seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+        let formatedHours = '';
+        let formatedMinutes = '';
+        let formatedSeconds = '';
+
+        if (hours < 10) formatedHours = '0'+hours;
+        else formatedHours = hours.toString();
+        if (minutes < 10) formatedMinutes = '0'+minutes;
+        else formatedMinutes = minutes.toString();
+        if (seconds < 10) formatedSeconds = '0'+seconds;
+        else formatedSeconds = seconds.toString();
+
+        return formatedHours+':'+formatedMinutes+':'+formatedSeconds;
+    }
 }
