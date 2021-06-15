0e5159fedf6f66c6f2bb13503db6eed61e70ec21
Jourloy <jourloy@yandex.ru>
2021-06-15 13:23:57 +0300

    Add defence from spamming by voice channels

--- src/modules/discord/discord.ts
+++ src/modules/discord/discord.ts
@@ -1,9 +1,11 @@
 /* IMPORTS */
 import { manager } from "../database/main";
 import { client, _jourloy } from "./main";
+import { logs } from "../tools/logs";
 
 import * as ds from 'discord.js';
 import * as _ from "lodash";
+import { tools } from "../tools/main";
 
 /* PARAM */
 const voiceChannels = {
@@ -24,8 +26,36 @@ const voiceChannels = {
         name: 'Game room [5]',
     },
 }
+let voiceUsers: string[] = [];
+let banVoiceUsers: string[] = [];
 
 /* INTERVALS */
+/**
+ * Clear voice users array every 5 minutes
+ */
+setInterval(() => {
+    voiceUsers = [];
+    banVoiceUsers = [];
+}, tools.convertTime({minutes: 2}));
+
+/** */
+setInterval(() => {
+    const warningsID = {};
+    for (let i in voiceUsers) {
+        if (warningsID[voiceUsers[i]] == null) {
+            warningsID[voiceUsers[i]] = {count: 1};
+        } else if (warningsID[voiceUsers[i]] != null) {
+            warningsID[voiceUsers[i]].count++;
+        }
+    }
+    for (let i in warningsID) {
+        if (warningsID[i].count > 3) {
+            logs.add(`${i} created too many channels`);
+            banVoiceUsers.push(i);
+        }
+    }
+})
+
 /**
  * Remove unused and undeleted channels
  */
@@ -34,17 +64,13 @@ setInterval(() => {
     const channelsArray = _jourloy.guild.channels.cache.array();
     const channels: ds.GuildChannel[] = [];
     for (let i in channelsArray) {
-        if (channelsArray[i].type === 'voice' && (channelsArray[i].name === voiceChannels.duo.name || channelsArray[i].name === voiceChannels.trio.name || channelsArray[i].name === voiceChannels.four.name)) channels.push(channelsArray[i]);
+        if (channelsArray[i].type === 'voice' && (channelsArray[i].name === voiceChannels.duo.name || channelsArray[i].name === voiceChannels.trio.name || channelsArray[i].name === voiceChannels.four.name || channelsArray[i].name === voiceChannels.five.name)) channels.push(channelsArray[i]);
     }
     for (let i in channels) {
         if (channels[i].members.array().length === 0) {
             channels[i].delete()
-                .then(() => {
-                    manager.nvyAddLog(`[LOG] -> Delete created channel`);
-                })
-                .catch(() => {
-                    manager.nvyAddLog(`[ERROR LOG] -> [MEDIUM] -> Can't delete created channel`);
-                });
+                .then(() => { logs.add(`Delete created channel via interval`) })
+                .catch(() => {});
         }
     }
 }, 10000)
@@ -57,12 +83,8 @@ setInterval(() => {
     const deleteFunction = (channelNew: ds.GuildChannel) => {
         if (channelNew.members.array().length === 0) {
             channelNew.delete()
-            .then(() => {
-                manager.nvyAddLog(`[LOG] -> Delete created channel`);
-            })
-            .catch(() => {
-                manager.nvyAddLog(`[ERROR LOG] -> [MEDIUM] -> Can't delete created channel`);
-            });
+            .then(() => { logs.add(`Delete created channel via function`) })
+            .catch(() => {});
             return true;
         }
         return false;
@@ -92,29 +114,38 @@ setInterval(() => {
             parent: parent,
         }
         const user = channel.members.array()[0].user;
-        let userVoiceState: ds.VoiceState = null;
-        let channelNew: ds.GuildChannel = null;
-        let idNew: string = null;
-
-        guild.members.fetch(user.id).then(member => {
-            for (let i in member.guild.voiceStates.cache.array()) {
-                if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
-            }
-
-            guild.channels.create(name, options).then(data => {
-                idNew = data.id;
-                const guildChannels = guild.channels.cache.array();
-                for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
-                userVoiceState.setChannel(channelNew)
-                    .then(res => {
-                        manager.nvyAddLog(`[LOG] -> Create channel for ${user.id} with limit 2`);
-                    })
-                    .catch(err => {
-                        manager.nvyAddLog(`[ERROR LOG] -> [LOW] -> ${user.id} fast leave from channel`);
-                    });
-                repeatCheck(channelNew);
+        voiceUsers.push(user.id);
+        if (banVoiceUsers.includes(user.id) === true) {
+            let userVoiceState: ds.VoiceState = null;
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+                userVoiceState.kick(`Created too many channels`);
+                logs.add(`Kick ${user.id} from voice channel`, true);
+                return;
             });
-        });
+        } else {
+            let userVoiceState: ds.VoiceState = null;
+            let channelNew: ds.GuildChannel = null;
+            let idNew: string = null;
+
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+
+                guild.channels.create(name, options).then(data => {
+                    idNew = data.id;
+                    const guildChannels = guild.channels.cache.array();
+                    for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
+                    userVoiceState.setChannel(channelNew)
+                        .then(res => { logs.add(`Create channel for ${user.id} with limit 2`) })
+                        .catch(err => { logs.add(`${user.id} fast leaved from channel`, true, true) });
+                    repeatCheck(channelNew);
+                });
+            });
+        }
     });
 
     /**
@@ -133,29 +164,38 @@ setInterval(() => {
             parent: parent,
         }
         const user = channel.members.array()[0].user;
-        let userVoiceState: ds.VoiceState = null;
-        let channelNew: ds.GuildChannel = null;
-        let idNew: string = null;
-
-        guild.members.fetch(user.id).then(member => {
-            for (let i in member.guild.voiceStates.cache.array()) {
-                if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
-            }
-
-            guild.channels.create(name, options).then(data => {
-                idNew = data.id;
-                const guildChannels = guild.channels.cache.array();
-                for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
-                userVoiceState.setChannel(channelNew)
-                    .then(res => {
-                        manager.nvyAddLog(`[LOG] -> Create channel for ${user.id} with limit 3`);
-                    })
-                    .catch(err => {
-                        manager.nvyAddLog(`[ERROR LOG] -> [LOW] -> ${user.id} fast leave from channel`);
-                    });
-                repeatCheck(channelNew);
+        voiceUsers.push(user.id);
+        if (banVoiceUsers.includes(user.id) === true) {
+            let userVoiceState: ds.VoiceState = null;
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+                userVoiceState.kick(`Created too many channels`);
+                logs.add(`Kick ${user.id} from voice channel`, true);
+                return;
             });
-        });
+        } else {
+            let userVoiceState: ds.VoiceState = null;
+            let channelNew: ds.GuildChannel = null;
+            let idNew: string = null;
+
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+
+                guild.channels.create(name, options).then(data => {
+                    idNew = data.id;
+                    const guildChannels = guild.channels.cache.array();
+                    for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
+                    userVoiceState.setChannel(channelNew)
+                        .then(res => { logs.add(`Create channel for ${user.id} with limit 3`) })
+                        .catch(err => { logs.add(`${user.id} fast leaved from channel`, true, true) });
+                    repeatCheck(channelNew);
+                });
+            });
+        }
     });
 
     /**
@@ -174,29 +214,38 @@ setInterval(() => {
             parent: parent,
         }
         const user = channel.members.array()[0].user;
-        let userVoiceState: ds.VoiceState = null;
-        let channelNew: ds.GuildChannel = null;
-        let idNew: string = null;
-
-        guild.members.fetch(user.id).then(member => {
-            for (let i in member.guild.voiceStates.cache.array()) {
-                if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
-            }
-
-            guild.channels.create(name, options).then(data => {
-                idNew = data.id;
-                const guildChannels = guild.channels.cache.array();
-                for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
-                userVoiceState.setChannel(channelNew)
-                    .then(res => {
-                        manager.nvyAddLog(`[LOG] -> Create channel for ${user.id} with limit 4`);
-                    })
-                    .catch(err => {
-                        manager.nvyAddLog(`[ERROR LOG] -> [LOW] -> ${user.id} fast leave from channel`);
-                    });
-                repeatCheck(channelNew);
+        voiceUsers.push(user.id);
+        if (banVoiceUsers.includes(user.id) === true) {
+            let userVoiceState: ds.VoiceState = null;
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+                userVoiceState.kick(`Created too many channels`);
+                logs.add(`Kick ${user.id} from voice channel`, true);
+                return;
             });
-        });
+        } else {
+            let userVoiceState: ds.VoiceState = null;
+            let channelNew: ds.GuildChannel = null;
+            let idNew: string = null;
+
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+
+                guild.channels.create(name, options).then(data => {
+                    idNew = data.id;
+                    const guildChannels = guild.channels.cache.array();
+                    for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
+                    userVoiceState.setChannel(channelNew)
+                        .then(res => { logs.add(`Create channel for ${user.id} with limit 4`) })
+                        .catch(err => { logs.add(`${user.id} fast leaved from channel`, true, true) });
+                    repeatCheck(channelNew);
+                });
+            });
+        }
     });
 
     /**
@@ -215,29 +264,38 @@ setInterval(() => {
             parent: parent,
         }
         const user = channel.members.array()[0].user;
-        let userVoiceState: ds.VoiceState = null;
-        let channelNew: ds.GuildChannel = null;
-        let idNew: string = null;
-
-        guild.members.fetch(user.id).then(member => {
-            for (let i in member.guild.voiceStates.cache.array()) {
-                if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
-            }
-
-            guild.channels.create(name, options).then(data => {
-                idNew = data.id;
-                const guildChannels = guild.channels.cache.array();
-                for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
-                userVoiceState.setChannel(channelNew)
-                    .then(res => {2
-                        manager.nvyAddLog(`[LOG] -> Create channel for ${user.id} with limit 5`);
-                    })
-                    .catch(err => {
-                        manager.nvyAddLog(`[ERROR LOG] -> [LOW] -> ${user.id} fast leave from channel`);
-                    });
-                repeatCheck(channelNew);
+        voiceUsers.push(user.id);
+        if (banVoiceUsers.includes(user.id) === true) {
+            let userVoiceState: ds.VoiceState = null;
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+                userVoiceState.kick(`Created too many channels`);
+                logs.add(`Kick ${user.id} from voice channel`, true);
+                return;
             });
-        });
+        } else {
+            let userVoiceState: ds.VoiceState = null;
+            let channelNew: ds.GuildChannel = null;
+            let idNew: string = null;
+
+            guild.members.fetch(user.id).then(member => {
+                for (let i in member.guild.voiceStates.cache.array()) {
+                    if (user.id === member.guild.voiceStates.cache.array()[i].id) userVoiceState = member.guild.voiceStates.cache.array()[i];
+                }
+
+                guild.channels.create(name, options).then(data => {
+                    idNew = data.id;
+                    const guildChannels = guild.channels.cache.array();
+                    for (let i in guildChannels) if (guildChannels[i].id === idNew) channelNew = guildChannels[i];
+                    userVoiceState.setChannel(channelNew)
+                        .then(res => { logs.add(`Create channel for ${user.id} with limit 5`) })
+                        .catch(err => { logs.add(`${user.id} fast leaved from channel`, true, true) });
+                    repeatCheck(channelNew);
+                });
+            });
+        }
     });
 
 }, 1000)
@@ -260,7 +318,7 @@ client.on('message', msg => {
     const authorID = msg.author.id;
     const message = msg.content;
     const messageSplit = message.split(' ');
-    const command = messageSplit[0].split('%')[1];
+    const command = messageSplit[0].split('!')[1];
 })
 
 client.on('messageReactionAdd', (msg) => {
@@ -268,4 +326,8 @@ client.on('messageReactionAdd', (msg) => {
     const emoji = msg.emoji.name;
 
     if (msg.me === true) return;
+})
+
+client.on('channelCreate', (data) => {
+    manager.jrlyAddLog('')
 })
