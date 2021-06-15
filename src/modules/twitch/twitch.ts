9a8337b129278c98b42658d19bd3b244b6227743
Jourloy <jourloy@yandex.ru>
2021-06-16 02:25:29 +0300

    Add new commands

--- src/modules/twitch/twitch.ts
+++ src/modules/twitch/twitch.ts
@@ -1,9 +1,12 @@
 /* IMPORTS */
 import { discord } from "../discord/discord";
+import { twitchFetch } from "./twitchFetch";
 import { manager } from "../database/main";
+import { callOfDuty } from "../COD/main";
 import { tools } from "../tools/main";
 import { client } from "./main";
 import * as upt from "./uptime";
+import "../COD/main"
 import "./uptime";
 
 import * as _ from "lodash";
@@ -15,11 +18,35 @@ import * as _ from "lodash";
 /* INTERVALS */
 
 /* REACTIONS */
-
 client.on('message', (channel, userstate, message, self) => {
     if (self) return;
     const username = userstate['username'].toLowerCase();
     const messageSplit = message.split(' ');
     const msSplit = messageSplit[0].split('!');
     const command = msSplit[1];
+
+    /* GENERAL */
+
+    if (command === 'test') {
+        twitchFetch.getCurrentViewers().then(data => console.log(data));
+    } else if (command === 'watchtime') {
+        twitchFetch.getUserID(username).then(id => {
+            manager.getChatterInfo(id).then(chatter => {
+                const seconds = chatter.watchTime;
+                const time = tools.toHHMMSS(seconds);
+                client.say(channel, `@${username}, your watchtime is ${time}`);
+            })
+        })
+    }
+
+    /* WARZONE */
+
+    if (command === 'wz_level') {
+        callOfDuty.getPlayerInfo().then(player => {
+            const percent = Math.round(player.levelXpGained * 100 / (player.levelXpGained + player.levelXpRemainder));
+            client.say(channel, `@${username}, now I have a ${player.level} level (Level progress: ${percent}%)`)
+        })
+    } else if (command === 'wz_kd') {
+
+    }
 })
