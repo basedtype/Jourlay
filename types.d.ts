dca2cf18f4d07ebef4620edcfc308b9a301b22ba
Jourloy <jourloy@yandex.ru>
2021-06-26 03:38:37 +0300

    Add new types

--- types.d.ts
+++ types.d.ts
@@ -32,3 +32,139 @@ declare namespace toolsOptions {
         mounths?: number;
     }
 }
+
+declare namespace callOfDuty {
+    interface mwWeapon {
+        muzzle: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        barrel: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        laser: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        optic: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        stock: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        underbarrel: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        ammunition: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        rearGrip: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        perk: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+    }
+
+    interface mwWeaponPistol {
+        muzzle: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        barrel: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        laser: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        optic: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        triggerAction: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        ammunition: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        rearGrip: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        perk: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+    }
+
+    interface mwWeaponSniperRifle {
+        muzzle: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        barrel: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        laser: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        optic: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        stock: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        underbarrel: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        rearGrip: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+        perk: {
+            exist: boolean;
+            count?: number;
+            name?: string;
+        },
+    }
+}
