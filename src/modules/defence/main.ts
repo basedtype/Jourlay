3579cde3d4ce3623f49bb86923342b6c292e8add
Jourloy <jourloy@yandex.ru>
2021-08-27 00:20:05 +0300

    Add defence from bots

--- src/modules/defence/main.ts
+++ src/modules/defence/main.ts
@@ -4,14 +4,14 @@ import { manager } from "../database/main";
 /* CLASSES */
 export class defence {
     public static async check(text: string, id: string): Promise<{bool: boolean, reason: string}> {
-        const chatter = await manager.getChatterInfo(id);
         const words = await manager.defenceGetWords();
+        for (let i in words) if (text.includes(words[i]) === true) return {bool: false, reason: words[i]};
+        return {bool: true, reason: ''};
+    }
 
-        for (let i in words) {
-            if (text.includes(words[i]) === true) {
-                if (chatter == null || chatter.watchTime == null || chatter.watchTime < 50) return {bool: false, reason: words[i]};
-            }
-        }
+    public static async checkBots(text: string): Promise<{bool: boolean, reason: string}> {
+        const words = await manager.defenceGetBots();
+        for (let i in words) if (text.includes(words[i]) === true) return {bool: false, reason: words[i]};
         return {bool: true, reason: ''};
     }
 }
