967d9a5175a01c961150083de519977a42be0dc7
Jourloy <jourloy@yandex.ru>
2021-09-18 00:57:59 +0300

    Update main.ts

--- src/modules/Steam/main.ts
+++ src/modules/Steam/main.ts
@@ -1,5 +1,14 @@
 /* IMPORTS */
 import fetch from "node-fetch";
+import { tools } from "../tools/main";
+
+export let allGames = null;
+
+setInterval(async () => {
+    const response = await steam.getAllGames();
+    if (response == null || response.applist == null || response.applist.apps == null) return;
+    allGames = response.applist.apps;
+}, tools.convertTime({hours: 1}))
 
 /* CLASSES */
 export class steam {
@@ -12,15 +21,29 @@ export class steam {
         return await fetch(url, options).then(res => res.json()).catch(() => {});
     }
 
-    static async getFeatured() {
+    public static async getAllGames() {
+        const url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/?';
+        const response = await this.getJson(url, this.options);
+        return response;
+    }
+
+    public static async getAppID(name: string) {
+        for (let i in allGames) if (allGames[i].name.toLowerCase() === name.toLowerCase()) return allGames[i].appid;
+        return null;
+    }
+
+    public static async getFeatured() {
         const url = 'https://store.steampowered.com/api/featuredcategories';
         const response = await this.getJson(url, this.options);
         return response;
     }
 
-    static async getAppDetails(appID: string) {
-        const url = `https://store.steampowered.com/api/appdetails?appids=${appID}`;
+    public static async getAppDetails(appID: string) {
+        const url = `https://store.steampowered.com/api/appdetails?appids=${appID}&cc=ru`;
         const response = await this.getJson(url, this.options);
         return response;
     }
 }
+
+
+steam.getAllGames().then(data => allGames = data.applist.apps);
