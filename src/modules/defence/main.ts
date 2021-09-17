28cd8878fe37937d94cbe23dfd6a823b37e39f6b
Jourloy <jourloy@yandex.ru>
2021-09-18 00:57:54 +0300

    Update main.ts

--- src/modules/defence/main.ts
+++ src/modules/defence/main.ts
@@ -3,15 +3,15 @@ import { manager } from "../database/main";
 
 /* CLASSES */
 export class defence {
-    public static async check(text: string, id: string): Promise<{bool: boolean, reason: string}> {
+    public static async check(text: string, id?: string): Promise<{bool: boolean, reason: string}> {
         const words = await manager.defenceGetWords();
-        for (let i in words) if (text.includes(words[i]) === true) return {bool: false, reason: words[i]};
+        for (let i in words) if (text.includes(`${words[i]}`) === true) return {bool: false, reason: words[i]};
         return {bool: true, reason: ''};
     }
 
     public static async checkBots(text: string): Promise<{bool: boolean, reason: string}> {
         const words = await manager.defenceGetBots();
-        for (let i in words) if (text.includes(words[i]) === true) return {bool: false, reason: words[i]};
+        for (let i in words) if (text.includes(`${words[i]}`) === true) return {bool: false, reason: words[i]};
         return {bool: true, reason: ''};
     }
 }
