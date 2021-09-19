d39b91dd417bc0ebcd8d68d75b25dfff7c0733c8
Jourloy <jourloy@yandex.ru>
2021-09-19 14:25:13 +0300

    Update twitchFetch.ts

--- src/modules/twitch/twitchFetch.ts
+++ src/modules/twitch/twitchFetch.ts
@@ -9,11 +9,13 @@ export class twitchFetch {
         headers: {
             'Accept': 'application/vnd.twitchtv.v5+json',
             'Client-ID': 'qetz5m3hw8vv6qsid3uobvl8kjotfk',
-        }
+        },
+        data: '',
     }
 
     private static async getJson(url: string, options) {
-        return await fetch(url, options).then(res => res.json()).catch(() => {});
+        return await fetch(url, options).
+            then(res => res.json()).catch(() => { });
     }
 
     public static async getUserID(username: string): Promise<null | string> {
@@ -23,11 +25,36 @@ export class twitchFetch {
         return response.users[0]._id;
     }
 
-    public static async get(url: string, options?) {
-        if (options == null) options = this.options;
-        const id = await this.getUserID('jourloy');
+    public static async get(url: string, options?, admin?: boolean) {
+        if (options == null) {
+            options = this.options;
+            const id = await this.getUserID('jourloy');
+            const bearer = await manager.getBearer(id);
+            options.headers['Authorization'] = `Bearer ${bearer}`
+        }
+        if (admin != null && admin === true) options.headers['Client-ID'] = 'gp762nuuoqcoxypju8c569th9wz7q5'
+        return await this.getJson(url, options);
+    }
+
+    public static async post(url: string, options?) {
+        if (options == null) {
+            options = this.options;
+            options.method = 'POST';
+            const id = '158466757';
+            const bearer = await manager.getBearer(id);
+            options.headers['Authorization'] = `Bearer ${bearer}`
+        }
+        return await this.getJson(url, options);
+    }
+
+    public static async patch(url: string, data) {
+        const options = this.options;
+        options.method = 'PATCH';
+        const id = '158466757';
         const bearer = await manager.getBearer(id);
         options.headers['Authorization'] = `Bearer ${bearer}`
+        options.data = data;
+        options.headers.Accept = 'application/json';
         return await this.getJson(url, options);
     }
 
