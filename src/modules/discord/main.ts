69e7abedcb2d9ec52e4520a70774ded3451831de
Jourloy <jourloy@yandex.ru>
2021-09-28 00:48:39 +0300

    Update main.ts

--- src/modules/discord/main.ts
+++ src/modules/discord/main.ts
@@ -1,38 +1,32 @@
 /* IMPORTS */
 import * as Multiprogress from 'multi-progress';
 import { discord } from "../Bots/DiscordBot";
-import { config } from "../../../types";
 
 import * as ds from 'discord.js';
 
 /* PARAMS */
 const multi = new Multiprogress(process.stderr);
-const bar = multi.newBar(' getting discord client [:bar] :percent :etas', {
+const bar = multi.newBar(' launch discord client [:bar] :percent :etas', {
     complete: '+',
     incomplete: '-',
     width: 30,
     total: 5
 });
 export let client: ds.Client = null;
-export let _jourloy: config.discordOptions = null;
-let counter = 0
-let getClient: boolean = false;
+export let _guild: ds.Guild = null;
 
 /* INTERVALS */
-let clientIntervalID = setInterval(() => {
-    if (client != null) clearInterval(clientIntervalID);
-    if (getClient === false) discord.connect('437601028662231040');
-    client = discord.client;
-    _jourloy = discord._jourloy;
-    getClient = true;
+const ticksIntervals = setInterval(() => {
+    bar.tick(1);
 }, 1000)
 
-let startIntervalID = setInterval(() => {
-    bar.tick(1)
-    counter++;
-    if (counter < 5) return;
-    if (client != null && _jourloy.guild != null) {
+/* CODE */
+setTimeout(() => {
+    discord.init().then(() => {
+        client = discord.client;
+        _guild = discord._guild;
+        clearInterval(ticksIntervals);
+        bar.update(1);
         require('./discord');
-        clearInterval(startIntervalID);
-    }
+    })
 }, 1000)
