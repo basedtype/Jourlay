b9fa7288bf7ca844da5d77f4cd4e978abfb59db8
Jourloy <jourloy@yandex.ru>
2021-10-30 19:49:34 +0300

    Update app.module.ts

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiModule } from './api/api.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { LoadFileMiddleware } from 'src/middleware/loadfiles.middleware';
+import { ProfileModule } from 'src/profile/profile.module';
 
 @Module({
-	imports: [ApiModule],
+	imports: [
+		DatabaseModule,
+		AuthModule,
+		ProfileModule
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(LoadFileMiddleware)
+			.forRoutes('www');
+	}
+}
