99f229aecbabcff974ab2156bf41bf98097d1461
Jourloy <jourloy@yandex.ru>
2021-10-31 23:27:49 +0300

    Create index.js

---
+++ www/profile/config/js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const but = document.getElementById('submit').addEventListener('click', () => {
+        document.forms['form'].submit();
+    })
+});
