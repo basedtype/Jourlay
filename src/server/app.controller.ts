ad038636532bd712b0ab2edbb913983008d466e8
Jourloy <jourloy@yandex.ru>
2021-10-31 23:27:41 +0300

    Update app.controller.ts

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -6,12 +6,12 @@ import { AppService } from './app.service';
 export class AppController {
 	constructor(private readonly appService: AppService) { }
 
-	@Get('/addAuth')
+	/* @Get('/addAuth')
 	//@UseGuards(AuthGuard)
 	async addAuth(@Query() query): Promise<string> {
 		if (query.login == null) throw new HttpException('Login required', HttpStatus.BAD_REQUEST);
 		else if (query.password == null) throw new HttpException('Password required', HttpStatus.BAD_REQUEST);
 		else if (query.role == null) throw new HttpException('Role required', HttpStatus.BAD_REQUEST);
 		return this.appService.addAuth(query.login, query.password, query.role);
-	}
+	} */
 }
