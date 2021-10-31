746bce8b59e97678b1ed9805182821461a8239f8
Jourloy <jourloy@yandex.ru>
2021-10-31 23:26:59 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './server/app.module';
 import * as cookieParser from 'cookie-parser';
+import { DiscordService } from './modules/discord/discord.service';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(cookieParser());
+
+	app.get(DiscordService).init();
+
 	await app.listen(80);
 }
 bootstrap();
