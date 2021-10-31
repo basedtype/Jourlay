6389e3b02982e107ee9625a118b4ee15134e5489
Jourloy <jourloy@yandex.ru>
2021-10-31 23:27:39 +0300

    Update profile.service.ts

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from "fs";
+import { BinanceService } from 'src/modules/binance/binance.service';
 
 @Injectable()
 export class ProfileService {
-    loadPage() {
+
+    constructor(private readonly binanceService: BinanceService) { }
+
+    loadPage(path: string) {
+        this.binanceService.getBalance('ETH');
+        if (path !== '/') return fs.readFileSync(`./www/profile${path}/index.html`, 'utf8');
         return fs.readFileSync('./www/profile/index.html', 'utf8');
     }
 }
