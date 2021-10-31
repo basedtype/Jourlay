299679b08cb217cfd9a5deca539eab02509063ce
Jourloy <jourloy@yandex.ru>
2021-10-31 23:27:37 +0300

    Update profile.module.ts

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { BinanceModule } from 'src/modules/binance/binance.module';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
+  imports: [DatabaseModule, BinanceModule],
   controllers: [ProfileController],
   providers: [ProfileService]
 })
