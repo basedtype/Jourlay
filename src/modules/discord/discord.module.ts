5a894d28db190dd35eaa01c69a5cf2a551ec122f
Jourloy <jourloy@yandex.ru>
2021-11-01 23:43:41 +0300

    Update discord.module.ts

--- src/modules/discord/discord.module.ts
+++ src/modules/discord/discord.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
 import { EgsModule } from '../egs/egs.module';
 import { ToolsModule } from '../tools/tools.module';
 import { DiscordService } from './discord.service';
 import { ScheduleModule } from '@nestjs/schedule'
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
 	imports: [
 		DatabaseModule, 
 		EgsModule, 
 		ToolsModule,
-		ScheduleModule,
 	],
 	providers: [DiscordService],
 	exports: [DiscordService]
