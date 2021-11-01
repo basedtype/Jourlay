026febb12c4153e63f3bb5232a52039a5a5770d1
Jourloy <jourloy@yandex.ru>
2021-11-01 23:43:49 +0300

    Update app.controller.ts

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -1,17 +1,13 @@
-import { Controller, Get, HttpCode, HttpException, HttpStatus, Query, Req, UseGuards, Response } from '@nestjs/common';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { Controller, Get, Query, Req, Response } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) { }
+	constructor(private readonly appService: AppService) {}
 
-	/* @Get('/addAuth')
-	//@UseGuards(AuthGuard)
-	async addAuth(@Query() query): Promise<string> {
-		if (query.login == null) throw new HttpException('Login required', HttpStatus.BAD_REQUEST);
-		else if (query.password == null) throw new HttpException('Password required', HttpStatus.BAD_REQUEST);
-		else if (query.role == null) throw new HttpException('Role required', HttpStatus.BAD_REQUEST);
-		return this.appService.addAuth(query.login, query.password, query.role);
-	} */
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
 }
