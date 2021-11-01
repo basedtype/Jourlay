c3dd8cbbe2b56f8d8d45948c170c3fa5ac6ae54c
Jourloy <jourloy@yandex.ru>
2021-11-01 23:43:29 +0300

    Update database.service.ts

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import * as mongodb from "mongodb";
 import * as crypto from "crypto";
 import { Config } from 'types';
+import * as mongodb from "mongodb"
 
 /* PARAMS */
 const uri = "mongodb://127.0.0.1:27017/";
 const mongo = new mongodb.MongoClient(uri);
 
-const configDatabase = mongo.db('config');
-const userCollection = configDatabase.collection('users');
-const configCollection = configDatabase.collection('config');
-
 @Injectable()
 export class DatabaseService {
+    private userCol() {
+        const configDatabase = mongo.db('config');
+        return configDatabase.collection('users');
+    }
+
+    private configCol() {
+        const configDatabase = mongo.db('config');
+        return configDatabase.collection('config');
+    }
+
     async addUser(login: string, password: string, role: string): Promise<'err' | 'done'> {
         await mongo.connect();
+        const userCollection = this.userCol();
         const pass = crypto.createHmac('sha256', password);
         await userCollection.findOneAndDelete({ login: login.toLocaleLowerCase() })
         const res = await userCollection.insertOne({ login: login, password: pass, role: [role] }).then((doc) => { if (!doc) return 'err'; else return 'done' });
@@ -24,6 +31,7 @@ export class DatabaseService {
 
     async checkUser(login: string, password: string): Promise<boolean> {
         await mongo.connect();
+        const userCollection = this.userCol();
         const pass = crypto.createHmac('sha256', password);
         const res = await userCollection.findOne({ login: login }).then(user => { if (!user || user.password !== pass) return false; else return true });
         await mongo.close();
@@ -32,6 +40,7 @@ export class DatabaseService {
 
     async addConfig(data: Config.Service): Promise<'err' | 'done'> {
         await mongo.connect();
+        const configCollection = this.configCol();
         await configCollection.findOneAndDelete({service: data.service, target: data.target});
         const res = await configCollection.insertOne(data).then((doc) => { if (!doc) return 'err'; else return 'done' });
         await mongo.close();
@@ -40,6 +49,7 @@ export class DatabaseService {
 
     async getConfig(service: string, target?: string): Promise<any> {
         await mongo.connect();
+        const configCollection = this.configCol();
         const res = await configCollection.findOne({service: service}).then(async (data: Config.Service) => {
             if (!data) return {};
             else if (target != null) {
