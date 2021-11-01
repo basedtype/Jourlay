3ea9d17450d211e5b8a1599b3e82fcd76df99dee
Jourloy <jourloy@yandex.ru>
2021-11-01 23:43:21 +0300

    Create configuration.module.ts

---
+++ src/configuration/configuration.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+import { ConfigurationService } from './configuration.service';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			load: [configuration],
+		}),
+	],
+	providers: [ConfigurationService],
+	exports: [ConfigurationService],
+})
+export class ConfigurationModule {}
