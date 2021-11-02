1a4e30504767338ae0164cf81f1a08aac1172920
Jourloy <jourloy@yandex.ru>
2021-11-02 19:31:30 +0300

    Update steam.module.ts

--- src/modules/steam/steam.module.ts
+++ src/modules/steam/steam.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { SteamService } from './steam.service';
 
 @Module({
-  providers: [SteamService]
+  providers: [SteamService],
+  exports: [SteamService]
 })
 export class SteamModule {}
