452e44a3244adccb3bc62d1216bc82cbe9fd7000
Jourloy <jourloy@yandex.ru>
2021-11-02 19:31:27 +0300

    Update gog.service.ts

--- src/modules/gog/gog.service.ts
+++ src/modules/gog/gog.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import * as fetch from "request-promise";
 
 @Injectable()
-export class GogService {}
+export class GogService {
+    private options = {
+        method: 'GET',
+        headers: {}
+    }
+
+    private async getJson(url: string, options) {
+        return JSON.parse(await fetch(url, options))
+    }
+
+    public async getSales() {
+        const url = 'https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&price=discounted&sort=popularity';
+        const response = await this.getJson(url, this.options);
+        const products = response.products;
+        let count = 0
+        let array = [];
+        for (let i in products) {
+            if (count > 11) continue;
+            array.push(products[i]);
+            count++
+        }
+        return array;
+    }
+}
