62fc0fa390cef54283c830bd8b997af0c8464d63
Jourloy <jourloy@yandex.ru>
2021-11-09 10:48:28 +0300

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,40 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'src/configuration/configuration';
+import { BinanceLog } from 'src/entity/binance.entity';
+import { DiscordUser } from 'src/entity/discordUser.entity';
+import { Log } from 'src/entity/log.entity';
+import { Service } from 'src/entity/services.entity';
 import { DatabaseService } from './database.service';
 
+const config = configuration();
+
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+	imports: [
+		TypeOrmModule.forRootAsync({
+			useFactory: () => {
+				return {
+					type: 'postgres',
+					host: config.postgres.host,
+					port: config.postgres.port,
+					username: config.postgres.account.login,
+					password: config.postgres.account.password,
+					database: config.postgres.database,
+					entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+					synchronize: config.postgres.synchronize,
+					migrationsRun: false,
+					logging: true,
+					logger: 'file',
+					migrations: [__dirname + '/database/migrations/**/*{.ts,.js}'],
+					cli: {
+						migrationsDir: 'src/database/migrations',
+					},
+				}
+			},
+		}),
+		TypeOrmModule.forFeature([BinanceLog, DiscordUser, Log, Service])
+	],
+	providers: [DatabaseService],
+	exports: [DatabaseService],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
