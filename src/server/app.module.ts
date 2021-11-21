23d0536c4059cfd12bd16f7167b41b661ceacd8f
Jourloy <jourloy@yandex.ru>
2021-11-21 18:43:40 +0300

    Update app.module.ts

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -13,21 +13,23 @@ import { AuthModule } from 'src/auth/auth.module';
 import { ProfileModule } from 'src/profile/profile.module';
 import { DiscordModule } from 'src/modules/discord/discord.module';
 import { LoadFileMiddleware } from 'src/middleware/loadfiles.middleware';
+import { AlisaModule } from 'src/modules/alisa/alisa.module';
 
 @Module({
 	imports: [
 		//TaskModule,
-		ConfigurationModule,
-		DatabaseModule,
-		ConfigModule,
-		BullModule,
-		AuthModule,
-		ProfileModule,
-		DiscordModule,
-		ConfigModule.forRoot({
-			load: [configuration],
-		}),
-		ScheduleModule.forRoot(),
+		//ConfigurationModule,
+		//DatabaseModule,
+		//ConfigModule,
+		//BullModule,
+		//AuthModule,
+		//ProfileModule,
+		//DiscordModule,
+		//ConfigModule.forRoot({
+		//	load: [configuration],
+		//}),
+		//ScheduleModule.forRoot(),
+		AlisaModule
 	],
 	controllers: [AppController],
 	providers: [AppService],
