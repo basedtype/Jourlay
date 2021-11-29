049499e8d350d9a1eb1d65185c3f73eedf3bc5e7
Jourloy <jourloy@yandex.ru>
2021-11-29 21:56:28 +0300

    Update alisa.module.ts

--- src/modules/alisa/alisa.module.ts
+++ src/modules/alisa/alisa.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AlisaService } from './alisa.service';
 import { AlisaController } from './alisa.controller';
+import { DiscordModule } from '../discord/discord.module';
 
 @Module({
+  imports: [DiscordModule],
   providers: [AlisaService],
   controllers: [AlisaController]
 })
