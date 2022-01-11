d870e4f0bde0a3d0efc900fbb613c14f960146c5
Igor Shaposhnikov <shaposhnikov.i@leadrock.com>
2022-01-12 01:09:30 +0300

    Remove nsfw

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,15 +1,12 @@
-import { Module } from "@nestjs/common";
-import { EgsModule } from "../modules/egs/egs.module";
-import { ToolsModule } from "../modules/tools/tools.module";
-import { DiscordService } from "./discord.service";
-import { ScheduleModule } from "@nestjs/schedule";
-import { DatabaseModule } from "src/database/database.module";
-import { SteamModule } from "../modules/steam/steam.module";
-import { GogModule } from "../modules/gog/gog.module";
-import { HtlbModule } from "../modules/htlb/htlb.module";
-import { AnimeModule } from "./modules/anime/anime.module";
-import { AmethystModule } from "../modules/amethyst/amethyst.module";
-import { WallhavenModule } from "../modules/wallhaven/wallhaven.module";
+import { Module } from '@nestjs/common';
+import { EgsModule } from '../modules/egs/egs.module';
+import { ToolsModule } from '../modules/tools/tools.module';
+import { DiscordService } from './discord.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { SteamModule } from '../modules/steam/steam.module';
+import { GogModule } from '../modules/gog/gog.module';
+import { HtlbModule } from '../modules/htlb/htlb.module';
+import { WallhavenModule } from '../modules/wallhaven/wallhaven.module';
 
 @Module({
 	imports: [
@@ -19,8 +16,6 @@ import { WallhavenModule } from "../modules/wallhaven/wallhaven.module";
 		GogModule,
 		HtlbModule,
 		ToolsModule,
-		AnimeModule,
-		AmethystModule,
 		WallhavenModule,
 	],
 	providers: [DiscordService],
