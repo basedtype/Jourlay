a9144d3c1d8a149d7fe1f29cea53fd8f7f16d3eb
Jourloy <jourloy@yandex.ru>
2022-05-15 08:34:09 +0300

    🗑 Remove unused

--- src/discord/modules/game.ts
+++ src/discord/modules/game.ts
@@ -1,14 +1,5 @@
-import { DatabaseService } from "src/database/database.service";
-
-
 export class Game {
-    private currency = 'Золото';
+	private currency = "Золото";
 
-    constructor (
-        private readonly databaseService = DatabaseService,
-    ) {}
-
-    async initPlayer() {
-        
-    }
+	async initPlayer() {}
 }
