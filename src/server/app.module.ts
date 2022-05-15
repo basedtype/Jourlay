db284f2e693654bd39fab4a25012f4dd6842de35
Jourloy <jourloy@yandex.ru>
2022-05-15 08:36:54 +0300

    🔨 Fix cron

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,10 +1,12 @@
 /* IMPORT */
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 import { DiscordModule } from "src/discord/discord.module";
 
 @Module({
 	imports: [
 		DiscordModule,
+		ScheduleModule.forRoot(),
 	],
 	controllers: [],
 	providers: [],
