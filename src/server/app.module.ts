85214f43d8cc87ebbe8fdb341326f4e5d1245249
Jourloy <jourloy@yandex.ru>
2022-05-15 08:32:03 +0300

    🗑 Remove unused

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,35 +1,12 @@
 /* IMPORT */
-import { ConfigurationModule } from "../configuration/configuration.module";
-import { DatabaseModule } from "../database/database.module";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ScheduleModule } from "@nestjs/schedule";
-import { ConfigModule } from "@nestjs/config";
-import { BullModule } from "@nestjs/bull";
-import { MiddlewareConsumer, Module } from "@nestjs/common";
-import configuration from "src/configuration/configuration";
-import { AuthModule } from "src/auth/auth.module";
-import { ProfileModule } from "src/profile/profile.module";
+import { Module } from "@nestjs/common";
 import { DiscordModule } from "src/discord/discord.module";
-import { LoadFileMiddleware } from "src/middleware/loadfiles.middleware";
 
 @Module({
 	imports: [
-		ConfigurationModule,
-		ConfigModule,
-		AuthModule,
-		ProfileModule,
 		DiscordModule,
-		ConfigModule.forRoot({
-			load: [configuration],
-		}),
-		ScheduleModule.forRoot(),
 	],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
-export class AppModule {
-	configure(consumer: MiddlewareConsumer) {
-		consumer.apply(LoadFileMiddleware).forRoutes("www");
-	}
-}
+export class AppModule {}
